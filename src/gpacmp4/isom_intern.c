f31425e9f0983ad6a1cc77348e4655a9487d861f
Ruslan Kuchumov <kuchumovri@gmail.com>
2014-07-28 13:57:54 +0000

    cygwin support

--- src/gpacmp4/isom_intern.c
+++ src/gpacmp4/isom_intern.c
@@ -398,23 +398,6 @@ GF_ISOFile *gf_isom_open_file(const char *fileName, u32 OpenMode, const char *tm
 	return mov;
 }
 
-#ifndef WIN32
-void gf_utc_time_since_1970(u32 *sec, u32 *msec)
-{
-#if defined (WIN32) && !defined(_WIN32_WCE)
-	struct _timeb	tb;
-	_ftime( &tb );
-	*sec = (u32) tb.time;
-	*msec = tb.millitm;
-#else
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	*sec = tv.tv_sec;
-	*msec = tv.tv_usec/1000;
-#endif
-}
-#endif
-
 u64 gf_isom_get_mp4time()
 {
 	u32 calctime, msec;
