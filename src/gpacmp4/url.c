ec427fd82c206bdd757595a87a26da92c8a04336
Anshul Maheshwari <er.anshul.maheshwari@gmail.com>
2014-10-07 00:21:28 -0700

    remove use of relative path

--- src/gpacmp4/url.c
+++ src/gpacmp4/url.c
@@ -21,7 +21,7 @@
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
  *
  */
-#include "../disable_warnings.h"
+#include "disable_warnings.h"
 #include <gpac/network.h>
 
 /* the length of the URL separator ("://" || "|//") */
