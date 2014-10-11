63b209929e07378ff0ddc61230f9a51f79c068e3
cfsmp3 <cfsmp3@gmail.com>
2014-10-11 10:48:12 +0200

    Version push (to 0.75)

--- src/lib_ccx/lib_ccx.h
+++ src/lib_ccx/lib_ccx.h
@@ -1,7 +1,7 @@
 #ifndef CCX_CCEXTRACTOR_H
 #define CCX_CCEXTRACTOR_H
 
-#define VERSION "0.74"
+#define VERSION "0.75"
 
 // Load common includes and constants for library usage
 #include "ccx_common_platform.h"
