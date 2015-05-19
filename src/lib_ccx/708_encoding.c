2ed9789f9c40e82f289c2baecab13f30a0eb63ad
Anshul Maheshwari <er.anshul.maheshwari@gmail.com>
2015-05-19 11:14:57 +0530

    Indenting 708_encoding

--- src/lib_ccx/708_encoding.c
+++ src/lib_ccx/708_encoding.c
@@ -14,32 +14,32 @@ A0-FF -> Group G1 as is - non-English characters and symbols
 
 unsigned char get_internal_from_G0 (unsigned char g0_char)
 {
-    return g0_char;
+	return g0_char;
 }
 
 unsigned char get_internal_from_G1 (unsigned char g1_char)
 {
-    return g1_char;
+	return g1_char;
 }
 
 // TODO: Probably not right
 // G2: Extended Control Code Set 1
 unsigned char get_internal_from_G2 (unsigned char g2_char)
 {
-    if (g2_char>=0x20 && g2_char<=0x3F)
-        return g2_char-0x20;
-    if (g2_char>=0x60 && g2_char<=0x7F)
-        return g2_char+0x20;
-    // Rest unmapped, so we return a blank space
-    return 0x20;
+	if (g2_char>=0x20 && g2_char<=0x3F)
+		return g2_char-0x20;
+	if (g2_char>=0x60 && g2_char<=0x7F)
+		return g2_char+0x20;
+	// Rest unmapped, so we return a blank space
+	return 0x20;
 }
 
 // TODO: Probably not right
 // G3: Future Characters and Icon Expansion
 unsigned char get_internal_from_G3 (unsigned char g3_char)
 {
-    if (g3_char==0xa0) // The "CC" (closed captions) sign
-        return 0x06;
-    // Rest unmapped, so we return a blank space
-    return 0x20;
+	if (g3_char==0xa0) // The "CC" (closed captions) sign
+		return 0x06;
+	// Rest unmapped, so we return a blank space
+	return 0x20;
 }
