fb2f909bd35dee2777229ce37ac9e1b75b222987
Oleg Kisselef <olegkisselef@gmail.com>
2015-06-22 23:51:49 +0600

    cctranslate launching

--- src/ccextractor.c
+++ src/ccextractor.c
@@ -279,6 +279,9 @@ int main(int argc, char *argv[])
 	signal_ctx = ctx;
 	m_signal(SIGINT, sigint_handler);
 #endif
+    if (ccx_options.sharing_enabled) {
+        ccx_share_launch_translator("fr,it", "AIzaSyC5GVfnYMqPs-1fGGl4-AO52ct798SPU0Q");
+    }
 
 	while (switch_to_next_file(ctx, 0) && !dec_ctx->processed_enough)
 	{
