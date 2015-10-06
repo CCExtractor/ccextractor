669eb366037f49b95c1740a4dd607783eab0fce0
Anshul Maheshwari <anshul.ffmpeg@gmail.com>
2015-10-06 08:54:09 +0200

    remove redundant logs
    Signed-off-by: Anshul Maheshwari <anshul.ffmpeg@gmail.com>

--- src/lib_ccx/ccx_decoders_608.c
+++ src/lib_ccx/ccx_decoders_608.c
@@ -376,7 +376,6 @@ int write_cc_line(ccx_decoder_608_context *context, struct cc_subtitle *sub)
 		}
 		memcpy(((struct eia608_screen *)sub->data) + sub->nb_data, data, sizeof(*data));
 		data = (struct eia608_screen *)sub->data + sub->nb_data;
-		ccx_common_logging.log_ftn("nb _data %d\n", sub->nb_data);
 		sub->nb_data++;
 
 		for(i = 0; i < 15; i++)
