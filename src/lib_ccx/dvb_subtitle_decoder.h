cd3cc2e26213a4cfe7e51d961e24acae58f78902
kerct <h1310076@nushigh.edu.sg>
2016-12-10 20:56:00 +0800

    Update dvb_subtitle_decoder.h

--- src/lib_ccx/dvb_subtitle_decoder.h
+++ src/lib_ccx/dvb_subtitle_decoder.h
@@ -20,20 +20,18 @@
 
 #include "lib_ccx.h"
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-struct dvb_config
-{
-	unsigned char n_language;
-	unsigned int lang_index[MAX_LANGUAGE_PER_DESC];
-	/* subtitle type */
-	unsigned char sub_type[MAX_LANGUAGE_PER_DESC];
-	/* composition page id */
-	unsigned short composition_id[MAX_LANGUAGE_PER_DESC];
-	/* ancillary_page_id */
-	unsigned short ancillary_id[MAX_LANGUAGE_PER_DESC];
+struct dvb_config {
+  unsigned char n_language;
+  unsigned int lang_index[MAX_LANGUAGE_PER_DESC];
+  /* subtitle type */
+  unsigned char sub_type[MAX_LANGUAGE_PER_DESC];
+  /* composition page id */
+  unsigned short composition_id[MAX_LANGUAGE_PER_DESC];
+  /* ancillary_page_id */
+  unsigned short ancillary_id[MAX_LANGUAGE_PER_DESC];
 };
 
 /**
@@ -42,33 +40,37 @@ struct dvb_config
  * @return DVB context kept as void* for abstraction
  *
  */
-void* dvbsub_init_decoder(struct dvb_config* cfg);
+void *dvbsub_init_decoder(struct dvb_config *cfg);
 
 int dvbsub_close_decoder(void **dvb_ctx);
 
 /**
  * @param dvb_ctx    PreInitialized DVB context using DVB
- * @param buf        buffer containing segment data, first sync byte need to 0x0f.
+ * @param buf        buffer containing segment data, first sync byte need to
+ * 0x0f.
  *                   does not include data_identifier and subtitle_stream_id.
  * @param buf_size   size of buf buffer
  * @param sub        output subtitle data
  *
  * @return           -1 on error
  */
-int dvbsub_decode(struct lib_cc_decode *dec_ctx, const unsigned char *buf, int buf_size, struct cc_subtitle *sub);
+int dvbsub_decode(struct lib_cc_decode *dec_ctx, const unsigned char *buf,
+                  int buf_size, struct cc_subtitle *sub);
 
 /**
  * @func parse_dvb_description
  *
- * data pointer to this function should be after description length and description tag is parsed
+ * data pointer to this function should be after description length and
+ * description tag is parsed
  *
- * @param cfg preallocated dvb_config structure where parsed description will be stored,Not to be NULL
+ * @param cfg preallocated dvb_config structure where parsed description will be
+ * stored,Not to be NULL
  *
  * @return return -1 if invalid data found other wise 0 if everything goes well
  * errno is set is to EINVAL if invalid data is found
  */
-int parse_dvb_description(struct dvb_config* cfg, unsigned char*data,
-		unsigned int len);
+int parse_dvb_description(struct dvb_config *cfg, unsigned char *data,
+                          unsigned int len);
 
 /*
  * @func dvbsub_set_write the output structure in dvb
