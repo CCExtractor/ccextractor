086b4db4b4ec9b960c1164222173669274dcf5fb
kerct <h1310076@nushigh.edu.sg>
2016-12-11 12:18:21 +0800

    Update file_buffer.h

--- src/lib_ccx/file_buffer.h
+++ src/lib_ccx/file_buffer.h
@@ -17,72 +17,87 @@
  * @return 0 or number of bytes, if returned 0 then op should check error number
  * to know
  *              details of error
- */
-size_t buffered_read_opt(struct ccx_demuxer *ctx, unsigned char *buffer,
-                         size_t bytes);
+ */ 
+  size_t buffered_read_opt (struct ccx_demuxer *ctx, unsigned char *buffer,
+                            size_t bytes);
 
 /**
  * Skip bytes from file buffer and if needed also seek file for number of bytes.
  *
- */
-static size_t inline buffered_skip(struct ccx_demuxer *ctx,
-                                   unsigned int bytes) {
+ */ 
+  static size_t inline
+buffered_skip (struct ccx_demuxer *ctx, unsigned int bytes)
+{
   size_t result;
-  if (bytes <= ctx->bytesinbuffer - ctx->filebuffer_pos) {
-    ctx->filebuffer_pos += bytes;
-    result = bytes;
-  } else {
-    result = buffered_read_opt(ctx, NULL, bytes);
-  }
+  if (bytes <= ctx->bytesinbuffer - ctx->filebuffer_pos)
+    {
+      ctx->filebuffer_pos += bytes;
+      result = bytes;
+    }
+  else
+    {
+      result = buffered_read_opt (ctx, NULL, bytes);
+    }
   return result;
 }
+
 
 /**
  * Read bytes from file buffer and if needed also read file for number of bytes.
  *
- */
-static size_t inline buffered_read(struct ccx_demuxer *ctx,
-                                   unsigned char *buffer, size_t bytes) {
+ */ 
+  static size_t inline
+buffered_read (struct ccx_demuxer *ctx, unsigned char *buffer, size_t bytes)
+{
   size_t result;
-  if (bytes <= ctx->bytesinbuffer - ctx->filebuffer_pos) {
-    if (buffer != NULL)
-      memcpy(buffer, ctx->filebuffer + ctx->filebuffer_pos, bytes);
-    ctx->filebuffer_pos += bytes;
-    result = bytes;
-  } else {
-    result = buffered_read_opt(ctx, buffer, bytes);
-    if (ccx_options.gui_mode_reports &&
-        ccx_options.input_source == CCX_DS_NETWORK) {
-      net_activity_gui++;
-      if (!(net_activity_gui % 1000)) activity_report_data_read();
+  if (bytes <= ctx->bytesinbuffer - ctx->filebuffer_pos)
+    {
+      if (buffer != NULL)
+        memcpy (buffer, ctx->filebuffer + ctx->filebuffer_pos, bytes);
+      ctx->filebuffer_pos += bytes;
+      result = bytes;
     }
-  }
+  else
+    {
+      result = buffered_read_opt (ctx, buffer, bytes);
+      if (ccx_options.gui_mode_reports
+           && ccx_options.input_source == CCX_DS_NETWORK)
+        {
+          net_activity_gui++;
+          if (!(net_activity_gui % 1000))
+            activity_report_data_read ();
+        }
+    }
   return result;
 }
+
 
 /**
  * Read single byte from file buffer and if needed also read file for number of
  * bytes.
  *
- */
-static size_t inline buffered_read_byte(struct ccx_demuxer *ctx,
-                                        unsigned char *buffer) {
+ */ 
+  static size_t inline
+buffered_read_byte (struct ccx_demuxer *ctx, unsigned char *buffer)
+{
   size_t result;
-  if (ctx->bytesinbuffer - ctx->filebuffer_pos) {
-    if (buffer) {
-      *buffer = ctx->filebuffer[ctx->filebuffer_pos];
-      ctx->filebuffer_pos++;
-      result = 1;
+  if (ctx->bytesinbuffer - ctx->filebuffer_pos)
+    {
+      if (buffer)
+        {
+          *buffer = ctx->filebuffer[ctx->filebuffer_pos];
+          ctx->filebuffer_pos++;
+          result = 1;
+        }
     }
-  } else
-    result = buffered_read_opt(ctx, buffer, 1);
-
+  else
+    result = buffered_read_opt (ctx, buffer, 1);
   return result;
 }
+unsigned short buffered_get_be16 (struct ccx_demuxer *ctx);
+unsigned char buffered_get_byte (struct ccx_demuxer *ctx);
+unsigned int buffered_get_be32 (struct ccx_demuxer *ctx);
+unsigned short buffered_get_le16 (struct ccx_demuxer *ctx);
+unsigned int buffered_get_le32 (struct ccx_demuxer *ctx);
 
-unsigned short buffered_get_be16(struct ccx_demuxer *ctx);
-unsigned char buffered_get_byte(struct ccx_demuxer *ctx);
-unsigned int buffered_get_be32(struct ccx_demuxer *ctx);
-unsigned short buffered_get_le16(struct ccx_demuxer *ctx);
-unsigned int buffered_get_le32(struct ccx_demuxer *ctx);
-#endif
+#endif  /*  */
