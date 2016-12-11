0302e452ed068af5c523f51f1cf35737861bc4b9
kerct <h1310076@nushigh.edu.sg>
2016-12-11 12:36:15 +0800

    Update ccx_encoders_helpers.h

--- src/lib_ccx/ccx_encoders_helpers.h
+++ src/lib_ccx/ccx_encoders_helpers.h
@@ -7,14 +7,15 @@
 #include "ccx_decoders_608.h"
 #include "ccx_encoders_common.h"
 
-extern char **spell_lower;
-extern char **spell_correct;
+extern char** spell_lower;
+extern char** spell_correct;
 extern int spell_words;
 extern int spell_capacity;
 
 extern unsigned char usercolor_rgb[8];
 
-struct ccx_encoders_helpers_settings_t {
+struct ccx_encoders_helpers_settings_t
+{
   int trim_subs;
   int no_font_color;
   int no_type_setting;
@@ -22,28 +23,28 @@ struct ccx_encoders_helpers_settings_t {
 };
 
 // Helper functions
-void correct_case_with_dictionary(int line_num, struct eia608_screen *data);
-int is_all_caps(struct encoder_ctx *context, int line_num,
-                struct eia608_screen *data);
-int clever_capitalize(struct encoder_ctx *context, int line_num,
-                      struct eia608_screen *data);
-void telx_correct_case(char *sub_line);
-unsigned get_decoder_line_encoded_for_gui(unsigned char *buffer, int line_num,
-                                          struct eia608_screen *data);
-unsigned get_decoder_line_encoded(struct encoder_ctx *ctx,
-                                  unsigned char *buffer, int line_num,
-                                  struct eia608_screen *data);
+void correct_case_with_dictionary(int line_num, struct eia608_screen* data);
+int is_all_caps(struct encoder_ctx* context, int line_num,
+                struct eia608_screen* data);
+int clever_capitalize(struct encoder_ctx* context, int line_num,
+                      struct eia608_screen* data);
+void telx_correct_case(char* sub_line);
+unsigned get_decoder_line_encoded_for_gui(unsigned char* buffer, int line_num,
+                                          struct eia608_screen* data);
+unsigned get_decoder_line_encoded(struct encoder_ctx* ctx,
+                                  unsigned char* buffer, int line_num,
+                                  struct eia608_screen* data);
 
-int string_cmp(const void *p1, const void *p2);
-int string_cmp2(const void *p1, const void *p2, void *arg);
+int string_cmp(const void* p1, const void* p2);
+int string_cmp2(const void* p1, const void* p2, void* arg);
 int add_built_in_words(void);
-int add_word(const char *word);
-unsigned encode_line(struct encoder_ctx *ctx, unsigned char *buffer,
-                     unsigned char *text);
+int add_word(const char* word);
+unsigned encode_line(struct encoder_ctx* ctx, unsigned char* buffer,
+                     unsigned char* text);
 
-void shell_sort(void *base, int nb, size_t size,
-                int (*compar)(const void *p1, const void *p2, void *arg),
-                void *arg);
+void shell_sort(void* base, int nb, size_t size,
+                int (*compar)(const void* p1, const void* p2, void* arg),
+                void* arg);
 
 void ccx_encoders_helpers_perform_shellsort_words(void);
 void ccx_encoders_helpers_setup(enum ccx_encoding_type encoding,
