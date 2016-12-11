02ec70d9357130ddda297e01636f079a83f4f6f6
kerct <h1310076@nushigh.edu.sg>
2016-12-11 12:34:24 +0800

    Update ccx_sub_entry_message.pb-c.h

--- src/lib_ccx/ccx_sub_entry_message.pb-c.h
+++ src/lib_ccx/ccx_sub_entry_message.pb-c.h
@@ -23,39 +23,40 @@ typedef struct _CcxSubEntryMessage CcxSubEntryMessage;
 
 /* --- messages --- */
 
-struct _CcxSubEntryMessage {
+struct _CcxSubEntryMessage
+{
   ProtobufCMessage base;
   int32_t eos;
-  char *stream_name;
+  char* stream_name;
   int64_t counter;
   int64_t start_time;
   int64_t end_time;
   size_t n_lines;
-  char **lines;
+  char** lines;
 };
-#define CCX_SUB_ENTRY_MESSAGE__INIT                             \
-  {                                                             \
-    PROTOBUF_C_MESSAGE_INIT(&ccx_sub_entry_message__descriptor) \
-    , 0, NULL, 0, 0, 0, 0, NULL                                 \
+#define CCX_SUB_ENTRY_MESSAGE__INIT                                            \
+  {                                                                            \
+    PROTOBUF_C_MESSAGE_INIT(&ccx_sub_entry_message__descriptor)                \
+    , 0, NULL, 0, 0, 0, 0, NULL                                                \
   }
 
 /* CcxSubEntryMessage methods */
-void ccx_sub_entry_message__init(CcxSubEntryMessage *message);
+void ccx_sub_entry_message__init(CcxSubEntryMessage* message);
 size_t ccx_sub_entry_message__get_packed_size(
-    const CcxSubEntryMessage *message);
-size_t ccx_sub_entry_message__pack(const CcxSubEntryMessage *message,
-                                   uint8_t *out);
-size_t ccx_sub_entry_message__pack_to_buffer(const CcxSubEntryMessage *message,
-                                             ProtobufCBuffer *buffer);
-CcxSubEntryMessage *ccx_sub_entry_message__unpack(ProtobufCAllocator *allocator,
+  const CcxSubEntryMessage* message);
+size_t ccx_sub_entry_message__pack(const CcxSubEntryMessage* message,
+                                   uint8_t* out);
+size_t ccx_sub_entry_message__pack_to_buffer(const CcxSubEntryMessage* message,
+                                             ProtobufCBuffer* buffer);
+CcxSubEntryMessage* ccx_sub_entry_message__unpack(ProtobufCAllocator* allocator,
                                                   size_t len,
-                                                  const uint8_t *data);
-void ccx_sub_entry_message__free_unpacked(CcxSubEntryMessage *message,
-                                          ProtobufCAllocator *allocator);
+                                                  const uint8_t* data);
+void ccx_sub_entry_message__free_unpacked(CcxSubEntryMessage* message,
+                                          ProtobufCAllocator* allocator);
 /* --- per-message closures --- */
 
-typedef void (*CcxSubEntryMessage_Closure)(const CcxSubEntryMessage *message,
-                                           void *closure_data);
+typedef void (*CcxSubEntryMessage_Closure)(const CcxSubEntryMessage* message,
+                                           void* closure_data);
 
 /* --- services --- */
 
@@ -65,6 +66,6 @@ extern const ProtobufCMessageDescriptor ccx_sub_entry_message__descriptor;
 
 PROTOBUF_C__END_DECLS
 
-#endif  // ENABLE_SHARING
+#endif // ENABLE_SHARING
 
 #endif /* PROTOBUF_C_ccx_5fsub_5fentry_5fmessage_2eproto__INCLUDED */
