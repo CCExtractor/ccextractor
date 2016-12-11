4fab7b4dce66786febd638edfab11f6c401df4a3
kerct <h1310076@nushigh.edu.sg>
2016-12-11 11:03:27 +0800

    Update ocr.h

--- src/lib_ccx/ocr.h
+++ src/lib_ccx/ocr.h
@@ -1,22 +1,23 @@
-#ifndef OCR_H
-#define OCR_H
-#include <png.h>
+#
+ifndef OCR_H# define OCR_H# include < png.h >
 
-struct image_copy  // A copy of the original OCR image, used for color detection
-    {
-  int nb_colors;
-  png_color *palette;
-  png_byte *alpha;
-  unsigned char *data;
-  int bgcolor;
-};
+  struct image_copy // A copy of the original OCR image, used for color detection
+  {
+    int nb_colors;
+    png_color * palette;
+    png_byte * alpha;
+    unsigned char * data;
+    int bgcolor;
+  };
 
-void delete_ocr(void **arg);
-void *init_ocr(int lang_index);
-char *ocr_bitmap(void *arg, png_color *palette, png_byte *alpha,
-                 unsigned char *indata, int w, int h, struct image_copy *copy);
-int ocr_rect(void *arg, struct cc_bitmap *rect, char **str, int bgcolor);
-char *paraof_ocrtext(struct cc_subtitle *sub, const char *crlf,
-                     unsigned crlf_length);
+void delete_ocr(void * * arg);
+void * init_ocr(int lang_index);
+char * ocr_bitmap(void * arg, png_color * palette, png_byte * alpha,
+  unsigned char * indata, int w, int h, struct image_copy * copy);
+int ocr_rect(void * arg, struct cc_bitmap * rect, char * * str, int bgcolor);
+char * paraof_ocrtext(struct cc_subtitle * sub,
+  const char * crlf,
+    unsigned crlf_length);
 
-#endif
+#
+endif
