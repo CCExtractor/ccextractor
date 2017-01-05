41537cf5edbc86fbd93e2c95e55f4ecaf7c89d9c
kalpana2k1 <kalpana.2k1@gmail.com>
2017-01-05 20:05:37 +0530

    Remove extra whitespaces and fix alignment.

--- src/lib_ccx/spupng_encoder.h
+++ src/lib_ccx/spupng_encoder.h
@@ -7,8 +7,8 @@
 #include "ccx_encoders_common.h"
 
 // CC page dimensions
-#define ROWS                    15
-#define COLUMNS                 32
+#define ROWS 15
+#define COLUMNS 32
 /* Closed Caption character cell dimensions */
 #define CCW 16
 #define CCH 26 /* line doubled */
@@ -27,7 +27,7 @@ struct spupng_t
 void write_spumux_header(struct encoder_ctx *ctx, struct ccx_s_write *out);
 void write_spumux_footer(struct ccx_s_write *out);
 void draw_char_indexed(uint8_t * canvas, int rowstride,  uint8_t * pen,
-		     int unicode, int italic, int underline);
+		       int unicode, int italic, int underline);
 void write_sputag_open(struct spupng_t *sp,LLONG ms_start,LLONG ms_end);
 void write_sputag_close(struct spupng_t *sp);
 void write_spucomment(struct spupng_t *sp,const char *str);
@@ -35,5 +35,5 @@ char* get_spupng_filename(void *ctx);
 void inc_spupng_fileindex(void *ctx);
 void set_spupng_offset(void *ctx,int x,int y);
 int mapclut_paletee(png_color *palette, png_byte *alpha, uint32_t *clut,
-		uint8_t depth);
+		    uint8_t depth);
 #endif
