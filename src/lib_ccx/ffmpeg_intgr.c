482a20430d59a620b555ebd39492f3f5e5b1b696
Carlos Fernandez <carlos@ccextractor.org>
2017-01-30 15:54:05 -0800

    ffmpeg_intgr.c: Wrong directory was being used in ffmpeg #include.

--- src/lib_ccx/ffmpeg_intgr.c
+++ src/lib_ccx/ffmpeg_intgr.c
@@ -3,7 +3,7 @@
 #ifdef ENABLE_FFMPEG
 #include <libavformat/avformat.h>
 #include <libavfilter/avfiltergraph.h>
-#include <libavfilter/avcodec.h>
+#include <libavcodec/avcodec.h>
 #include <libavfilter/buffersink.h>
 #include <libavfilter/buffersrc.h>
 #include <libavutil/avstring.h>
