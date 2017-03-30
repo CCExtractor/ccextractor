8878aebe0bf95877729d4da5be6e5b76f85bb2fc
Abhinav Baid <abhinavbaid@gmail.com>
2017-03-30 11:25:41 +0530

    Allow option to build .pkg.tar.xz archive without installing package

--- package_creators/arch.sh
+++ package_creators/arch.sh
@@ -2,7 +2,11 @@
 
 ./tarball.sh
 makepkg -g >> PKGBUILD
-makepkg -sic
-rm ./*.tar.gz
-rm ./*.pkg.tar.xz
+makepkg -sc
+rm -f ./*.tar.gz
 sed -i '$ d' PKGBUILD
+read -p "Do you wish to install ccextractor? [Y/n] " yn
+case $yn in
+    [Nn]* ) exit;;
+    * ) sudo pacman -U ./*.pkg.tar.xz; rm -f ./*.pkg.tar.xz;;
+esac
