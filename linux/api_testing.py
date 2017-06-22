2ed2c2790659c4a66062e7300e986c1a3c5a3014
Diptanshu8 <diptanshuj@gmail.com>
2017-06-22 15:22:14 +0000

    First phase evaluation  outputs generated

--- linux/api_testing.py
+++ linux/api_testing.py
@@ -1,12 +1,15 @@
 import ccextractorapi as cc
 import os
-
+import time
+import sys
+sample = " ".join(sys.argv[1:])
+print 'Name of sample is {0}'.format(sample)
+time.sleep(1)
 s =  cc.api_init_options()
 cc.checking_configuration_file(s)
-
-params = ['/vagrant/DISNEY.ts']
-for item in params:
-    cc.api_add_param(s,str(item))
+params = [str(sample)]
+for i in params:
+    cc.api_add_param(s,str(i))
 compile_ret = cc.compile_params(s,len(params));
 start_ret = cc.api_start(s);
-
+os.system('clear')
