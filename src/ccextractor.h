e8f742a6273b8d5d3c11493f3e0363a8c7721d64
Carlos Fernandez <carlos@ccextractor.org>
2017-07-14 13:01:39 -0700

    Corrected function prototype

--- src/ccextractor.h
+++ src/ccextractor.h
@@ -36,6 +36,6 @@ char * api_param(struct ccx_s_options* api_options, int count);
 
 void sigterm_handler(int sig);
 void sigint_handler(int sig);
-void print_end_msg();
+void print_end_msg(void);
 int main(int argc, char *argv[]);
 #endif //CCEXTRACTOR_H
