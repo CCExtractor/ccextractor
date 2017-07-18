10eb52e651cbb2f9e07bad34ad5ea220c3c2d2ca
Diptanshu8 <diptanshuj@gmail.com>
2017-07-18 11:19:14 +0000

    pushing 4 wrapper codes

--- api/wrappers/wrapper.h
+++ api/wrappers/wrapper.h
@@ -5,3 +5,4 @@ void setautoprogram(struct ccx_s_options *api_options);
 void setstdout(struct ccx_s_options *api_options);
 void setpesheader(struct ccx_s_options *api_options);
 void setdebugdbvsub(struct ccx_s_options *api_options);
+
