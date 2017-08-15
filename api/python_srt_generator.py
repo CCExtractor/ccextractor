1180dfeba11bc65cd7e2ab5ce1c6912dbb47f6ff
Diptanshu8 <diptanshuj@gmail.com>
2017-08-15 09:40:03 +0000

    Added the code to check for underline. However, need to check how CCExtractor generates srt when both italics and underline are present. For now a new line is added if both are present.

--- api/python_srt_generator.py
+++ api/python_srt_generator.py
@@ -27,18 +27,41 @@ def comparing_text_font_grids(text, font):
 
     for letter,font_line in zip(text,font):
         if "                                " not in letter:
+            buff=""
+            underline,italics = 0,0
+            #Handling underline
             buff = ""
-            flag = 0
+            underline_flag = 0
             for i,font_type in enumerate(font_line): 
-                if font_type == 'I' and not flag:
-                        buff = buff + '<i> '
-                        flag = 1
-                elif font_type =="R" and flag: 
-                        buff = buff + '</i>'
-                        flag = 0
+                if font_type == 'U' and not underline_flag:
+                        buff = buff + '<u> '
+                        underline_flag = 1
+                        underline=1
+                elif font_type =="R" and underline_flag: 
+                        buff = buff + '</u>'
+                        underline_flag = 0
                         continue;
                 buff +=  letter[i]
-            temp.append(buff)
+            #adding a new line after buff has seen underline
+            #need to cross check with CCExtractor output as to how they are doing
+            if underline:
+                buff+= "\n"
+            else:
+                buff=""
+
+            #Handling italics
+            italics_flag = 0
+            for i,font_type in enumerate(font_line): 
+                if font_type == 'I' and not italics_flag:
+                        buff = buff + '<i>'
+                        italics_flag = 1
+                elif font_type =="R" and italics_flag: 
+                        buff = buff + '</i>'
+                        italics_flag = 0
+                        continue;
+                buff +=  letter[i]
+            if buff!=letter:
+                temp.append(buff)
     print temp
 
     
