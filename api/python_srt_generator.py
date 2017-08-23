e58d132e22295cf416386c7953d0877afffdf683
Diptanshu8 <diptanshuj@gmail.com>
2017-08-23 17:00:30 +0000

    Added support for handling font and italics in Python SRT generator.

--- api/python_srt_generator.py
+++ api/python_srt_generator.py
@@ -21,10 +21,59 @@ import re
             else:
                 buff=""
 """
-def comparing_text_font_grids(text, font):
-    temp = []
-
-    for letter,font_line in zip(text,font):
+color_text_start={
+        "0":"",
+        "1":"<font color=\"#00ff00\">",
+        "2":"<font color=\"#0000ff\">",
+        "3":"<font color=\"#00ffff\">",
+        "4":"<font color=\"#ff0000\">",
+        "5":"<font color=\"#ffff00\">",
+        "6":"<font color=\"#ff00ff\">",
+        "7":"<font color=\"",
+        "8":"",
+        "9":""
+};
+color_text_end={
+        "0":"",
+        "1":"</font",
+        "2":"</font>",
+        "3":"</font>",
+        "4":"</font>",
+        "5":"</font>",
+        "6":"</font>",
+        "7":"</font>",
+        "8":"",
+        "9":""
+};
+def comparing_text_font_grids(text, font, color):
+    original_text = text
+    temp_color = []
+    for letter,color_line in zip(original_text,color):
+        color = 0
+        color_flag = 1
+        prev = color_line[0]
+        buff = color_text_start[str(prev)]
+        if "                                " not in letter:
+            for i,color_type in enumerate(color_line): 
+                if prev!=color_type and not color_flag:
+                        color = 1
+                        buff = buff + color_text_start[str(color_type)]
+                        color_flag = 1
+                elif prev!=color_type and color_flag: 
+                        color = 1
+                        buff = buff + color_text_end[str(prev)]
+                        color_flag = 0
+                buff += letter[i]
+                prev=color_type
+            if color_flag:
+                color_flag=0
+                buff+=color_text_end[str(prev)]
+        if color:
+            temp_color.append(buff)
+        else:
+            temp_color.append(letter)
+    temp_font_italics=[]
+    for letter,font_line in zip(original_text,font):
         if "                                " not in letter:
             buff=""
             underline,italics = 0,0
@@ -40,8 +89,19 @@ def comparing_text_font_grids(text, font):
                 buff +=  letter[i]
             if italics_flag:
                 buff+='</i>'
-            temp.append(buff)
-    return (temp,font)
+            temp_font_italics.append(buff)
+        else:
+            temp_font_italics.append(letter)
+    final = []
+    for i,j in zip(temp_color,temp_font_italics):
+        if i!=j:
+            if i in original_text:
+                final.append(j)
+            else:
+                final.append(i)
+        else:
+            final.append(i)
+    return (final,font,color)
 
     
 def generate_output_srt(filename,d):
@@ -49,8 +109,7 @@ def generate_output_srt(filename,d):
         d['text'] = [unicode(item,'utf-8') for item in d['text']]
     except:
         d['text'] = [unicode(item,'utf-8','ignore')[:32] for item in d['text']]
-        print d['text']
-    d['text'],d['font']= comparing_text_font_grids(d['text'],d['font'])
+    d['text'],d['font'],d['color']= comparing_text_font_grids(d['text'],d['font'],d['color'])
     for item in d['text']:
         if "                                " not in item:
             o=item
