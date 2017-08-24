aa62e57b479267f8af33ac8304abef121ef4898b
Diptanshu8 <diptanshuj@gmail.com>
2017-08-24 14:56:08 +0000

    added return code for failure of asprintf calls.

--- api/extractors/extractor.c
+++ api/extractors/extractor.c
@@ -8,6 +8,7 @@ void python_extract_g608_grid(unsigned h1, unsigned m1, unsigned s1, unsigned ms
      * identifier = 3 ---> font
      */
     int i;
+    int asprintf_ret;
     char* output;
     char* start_time = time_wrapper("%02u:%02u:%02u,%03u",h1,m1,s1,ms1);
     char* end_time = time_wrapper("%02u:%02u:%02u,%03u",h2,m2,s2,ms2);
@@ -17,21 +18,36 @@ void python_extract_g608_grid(unsigned h1, unsigned m1, unsigned s1, unsigned ms
             switch(identifier){
                 case 1:
 #if defined(PYTHONAPI) 
-                    asprintf(&output,"text[%d]:%s\n",srt_counter,buffer);
+                    asprintf_ret = asprintf(&output,"text[%d]:%s\n",srt_counter,buffer);
+                    if (asprintf_ret==-1)
+                    {
+                        printf("Error: Some problem with asprintf return value in extractor.c\nExiting.");
+                        exit(500);
+                    }
                     run(array.reporter,output);
                     free(output);
 #endif
                     return;
                  case 2:
 #if defined(PYTHONAPI) 
-                    asprintf(&output,"color[%d]:%s\n",srt_counter,buffer);
+                    asprintf_ret = asprintf(&output,"color[%d]:%s\n",srt_counter,buffer);
+                    if (asprintf_ret==-1)
+                    {
+                        printf("Error: Some problem with asprintf return value in extractor.c\nExiting.");
+                        exit(500);
+                    }
                     run(array.reporter,output);
                     free(output);
 #endif
                     return;
                  case 3:
 #if defined(PYTHONAPI) 
-                    asprintf(&output,"font[%d]:%s\n",srt_counter,buffer);
+                    asprintf_ret = asprintf(&output,"font[%d]:%s\n",srt_counter,buffer);
+                    if (asprintf_ret==-1)
+                    {
+                        printf("Error: Some problem with asprintf return value in extractor.c\nExiting.");
+                        exit(500);
+                    }
                     run(array.reporter,output);
                     free(output);
 #endif
@@ -39,7 +55,12 @@ void python_extract_g608_grid(unsigned h1, unsigned m1, unsigned s1, unsigned ms
                  case 4:
                  // writing end of frame 
 #if defined(PYTHONAPI) 
-                    asprintf(&output,"***END OF FRAME***\n",srt_counter,buffer);
+                    asprintf_ret = asprintf(&output,"***END OF FRAME***\n",srt_counter,buffer);
+                    if (asprintf_ret==-1)
+                    {
+                        printf("Error: Some problem with asprintf return value in extractor.c\nExiting.");
+                        exit(500);
+                    }
                     run(array.reporter,output);
                     free(output);
 #endif
@@ -58,10 +79,20 @@ void python_extract_g608_grid(unsigned h1, unsigned m1, unsigned s1, unsigned ms
     array.subs[array.sub_count-1].end_time = end_time;
     char* temp;
 #if defined(PYTHONAPI) 
-    asprintf(&output,"srt_counter-%d\n",srt_counter);
+    asprintf_ret = asprintf(&output,"srt_counter-%d\n",srt_counter);
+    if (asprintf_ret==-1)
+    {
+        printf("Error: Some problem with asprintf return value in extractor.c\nExiting.");
+        exit(500);
+    }
     run(array.reporter,output);
     free(output);
-    asprintf(&temp,"start_time-%s\t end_time-%s\n",start_time,end_time);
+    asprintf_ret = asprintf(&temp,"start_time-%s\t end_time-%s\n",start_time,end_time);
+    if (asprintf_ret==-1)
+    {
+        printf("Error: Some problem with asprintf return value in extractor.c\nExiting.");
+        exit(500);
+    }
     run(array.reporter,temp);  
     free(temp);
 #endif
--- api/extractors/extractor.h
+++ api/extractors/extractor.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ccextractor.h"
+#include "../../src/lib_ccx/ccx_common_common.h"
 
 void python_extract_g608_grid(unsigned h1, unsigned m1, unsigned s1, unsigned ms1, unsigned h2, unsigned m2, unsigned s2, unsigned ms2, char* buffer, int identifier, int srt_counter);
 
