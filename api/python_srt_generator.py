51739bc242e5724a4a1ad2d495bc56bed8897dd1
Diptanshu8 <diptanshuj@gmail.com>
2017-08-24 06:51:18 +0000

    modified the font generator. Also, added count method for checking blank strings in python_srt_generator.

--- api/python_srt_generator.py
+++ api/python_srt_generator.py
@@ -45,17 +45,22 @@ color_text_end={
         "8":"",
         "9":""
 };
+no_color_tag = ['0','8','9']
 def comparing_text_font_grids(text, font, color):
     original_text = text
+    original_color = color
     temp_color = []
     for letter,color_line in zip(original_text,color):
         color = 0
-        color_flag = 1
         prev = color_line[0]
         buff = color_text_start[str(prev)]
-        if "                                " not in letter:
+        if prev not in no_color_tag:
+            color_flag = 1
+        else:
+            color_flag = 0
+        if letter.count(" ")<32:
             for i,color_type in enumerate(color_line): 
-                if prev!=color_type and not color_flag:
+                if color_type not in no_color_tag and prev!=color_type and not color_flag:
                         color = 1
                         buff = buff + color_text_start[str(color_type)]
                         color_flag = 1
@@ -69,38 +74,45 @@ def comparing_text_font_grids(text, font, color):
                 color_flag=0
                 buff+=color_text_end[str(prev)]
         if color:
-            temp_color.append(buff)
+            temp_color.append((buff,1))
         else:
-            temp_color.append(letter)
+            temp_color.append((letter,0))
     temp_font_italics=[]
     for letter,font_line in zip(original_text,font):
-        if "                                " not in letter:
+        if letter.count(" ")<32:
             buff=""
             underline,italics = 0,0
             #Handling italics
             italics_flag = 0
             for i,font_type in enumerate(font_line): 
                 if font_type == 'I' and not italics_flag:
+                        italics=1
                         buff = buff + '<i>'
                         italics_flag = 1
                 elif font_type =="R" and italics_flag: 
+                        italics=1
                         buff = buff + '</i>'
                         italics_flag = 0
                 buff +=  letter[i]
             if italics_flag:
                 buff+='</i>'
-            temp_font_italics.append(buff)
+            if italics:
+                temp_font_italics.append((buff,1))
+            else:
+                temp_font_italics.append((letter,0))
         else:
-            temp_font_italics.append(letter)
+            temp_font_italics.append((letter,0))
     final = []
     for i,j in zip(temp_color,temp_font_italics):
-        if i!=j:
-            if i in original_text:
-                final.append(j)
-            else:
-                final.append(i)
+        if i[1] and not j[1]:
+            final.append(i[0])
+        elif j[1] and not i[1]:
+            final.append(j[0])
         else:
-            final.append(i)
+            if not i[1]:
+                final.append(i[0])
+            else:
+                print "error"
     return (final,font,color)
 
     
@@ -111,7 +123,7 @@ def generate_output_srt(filename,d):
         d['text'] = [unicode(item,'utf-8','ignore')[:32] for item in d['text']]
     d['text'],d['font'],d['color']= comparing_text_font_grids(d['text'],d['font'],d['color'])
     for item in d['text']:
-        if "                                " not in item:
+        if item.count(" ")<32:
             o=item
             with open(filename,'ab+') as fh:
                 fh.write(o.encode('utf8'))
