b3cd0fe7043688fc8516e2bc571e09c1bd4f4cda
Anubhav Chaudhary <achaudhary997@yahoo.in>
2018-02-26 13:15:49 +0530

    Fixed f717624b

--- src/lib_ccx/params.c
+++ src/lib_ccx/params.c
@@ -1,10 +1,10 @@
-#include "png.h"
-#include "protobuf-c.h"
-#include "utf8proc.h"
+#include "../libpng/png.h"
+#include "../protobuf-c/protobuf-c.h"
+#include "../utf8proc/utf8proc.h"
 #include "zlib.h"
-#include "gpac/version.h"
-#include "capi.h"
-#include "allheaders.h"
+#include "../gpacmp4/gpac/version.h"
+#include "../../windows/libs/include/capi.h"
+#include "../../windows/libs/include/allheaders.h"
 #include "lib_ccx.h"
 #include "ccx_common_option.h"
 #include "utility.h"
@@ -968,7 +968,7 @@ void version(char *location) {
 #endif
 	mprint("	libGPAC Version: %s\n", GPAC_VERSION);
 	mprint("	zlib: %s\n", ZLIB_VERSION);
-	mprint("	utf8proc Version: %s\n", (const char*) utf8proc_version());
+	// mprint("	utf8proc Version: %s\n", (const char*) utf8proc_version());
 	mprint("	protobuf-c Version: %s\n", (const char*) protobuf_c_version());
 	mprint("	libpng Version: %s\n", PNG_LIBPNG_VER_STRING);
 	mprint("	FreeType \n");
