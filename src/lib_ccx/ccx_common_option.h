eac6a98b133527b606d4f1c20016ba7f60492c62
MakarovGCI2018 <44312997+MakarovGCI2018@users.noreply.github.com>
2018-11-24 11:12:07 +0200

    Update ccx_common_option.h

--- src/lib_ccx/ccx_common_option.h
+++ src/lib_ccx/ccx_common_option.h
@@ -87,6 +87,7 @@ struct ccx_s_options // Options from user parameters
 	int no_rollup;
 	int noscte20;
 	int webvtt_create_css;
+	int noempty;
 	int cc_channel;                                            // Channel we want to dump in srt mode
 	int buffer_input;
 	int nofontcolor;
