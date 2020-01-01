2f95d1c71e0e7c852c1c37d9128804698bbd2291
eshan <eshandhawan51@gmail.com>
2020-01-02 03:49:20 +0530

    Removed invalid free condition for multiple files

--- src/gpacmp4/mp4.c
+++ src/gpacmp4/mp4.c
@@ -689,6 +689,7 @@ int processmp4 (struct lib_ccx_ctx *ctx, struct ccx_s_mp4Cfg *cfg, char *file)
 	}
 
 	free(dec_ctx->xds_ctx);
+	dec_ctx->xds_ctx=NULL;
 
 	mprint("\nClosing media: ");
 	gf_isom_close(f);
