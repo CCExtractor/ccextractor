6ca29460448069db527b563669c3cafc7b004df7
Dhrumil Patel <dhrumilp15@gmail.com>
2020-01-12 20:14:34 -0500

    Encoder checks its context

--- src/lib_ccx/ccx_encoders_webvtt.c
+++ src/lib_ccx/ccx_encoders_webvtt.c
@@ -215,7 +215,7 @@ int write_webvtt_header(struct encoder_ctx *context)
 		unsigned h1, m1, s1, ms1;
 		millis_to_time(context->timing->sync_pts2fts_fts, &h1, &m1, &s1, &ms1);
 		 
-		 // If the user has not disabled the X-TIMESTAMP-MAP header
+		 // If the user has not disabled X-TIMESTAMP-MAP
 		if (!context->no_timestamp_map)
 		{
 			sprintf(header_string, "X-TIMESTAMP-MAP=MPEGTS:%ld,LOCAL:%02u:%02u:%02u.%03u%s",
