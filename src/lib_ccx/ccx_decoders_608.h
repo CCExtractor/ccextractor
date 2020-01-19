d8ceb70d76e26074ac664b2a095506bb2b88cee4
Nils André-Chang <nils@nilsand.re>
2020-01-19 01:19:34 +0000

    b

--- src/lib_ccx/ccx_decoders_608.h
+++ src/lib_ccx/ccx_decoders_608.h
@@ -39,8 +39,8 @@ typedef struct ccx_decoder_608_context
 	enum cc_modes mode;
 	unsigned char last_c1, last_c2;
 	int channel;                    // Currently selected channel
-	unsigned char current_color;    // Color we are currently using to write
-	unsigned char font;             // Font we are currently using to write
+	enum ccx_decoder_608_color_code current_color;    // Color we are currently using to write
+	enum font_bits font;             // Font we are currently using to write
 	int rollup_base_row;
 	LLONG ts_start_of_current_line; /* Time at which the first character for current line was received, =-1 no character received yet */
 	LLONG ts_last_char_received;    /* Time at which the last written character was received, =-1 no character received yet */
