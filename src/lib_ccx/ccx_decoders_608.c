CCExtractor/ccextractor

commit 19241744d71adeda0f29c553b966236e88312c94

[FEATURE] SCC and CCD encoder (#1154)
* Fix indentation, use switch instead of if

* Remove confusing comment

Enums are abstractions and should be used as such. They shouldn't be
used like integers.

* Return a const char* instead of char * allocated on heap

* Test return value inline

* Add SCC output

* Add CCD format

* Add channel header to CCD

* Return const pointer

* Revert formatting change

* Colour -> Color

* Fix formatting

* Move comment to relevant place

* Improve readability

* Fix formatting

* Fix erroneous comment

* Use different parity function not requiring GNU extension

* Use enum instead of int

* Fix bug

* Implement channel functionality

* Fix CI errors

* Fix CI build

* Add options to help menu

* Mention change in changelog

* Add file to build systems

* Remove uneeded link against zlib

* Remove the use of <stdbool.h> and use const char

* Rewrite SCC formatter

* Use fdprintf

src/lib_ccx/ccx_decoders_608.c
@@ -104,7 +104,7 @@ const char *color_text[MAX_COLOR][2]=
 
 void clear_eia608_cc_buffer(ccx_decoder_608_context *context, struct eia608_screen *data)
 {
-	for (int i=0;i<15;i++)
+	for (int i = 0;i<15;i++)
 	{
 		memset(data->characters[i], ' ', CCX_DECODER_608_SCREEN_WIDTH);
 		data->characters[i][CCX_DECODER_608_SCREEN_WIDTH] = 0;
@@ -138,7 +138,7 @@ ccx_decoder_608_context* ccx_decoder_608_init_library(struct ccx_decoder_608_set
 	data->current_visible_start_ms=0;
 	data->screenfuls_counter=0;
 	data->channel=1;
-	data->font=FONT_REGULAR;
+	data->font = FONT_REGULAR;
 	data->rollup_base_row=14;
 	data->ts_start_of_current_line=-1;
 	data->ts_last_char_received=-1;
@@ -246,8 +246,8 @@ void handle_text_attr(const unsigned char c1, const unsigned char c2, ccx_decode
 	if (context->channel != context->my_channel)
 		return;
 	ccx_common_logging.debug_ftn(CCX_DMT_DECODER_608, "\r608: text_attr: %02X %02X", c1, c2);
-	if ( ((c1!=0x11 && c1!=0x19) ||
-		(c2<0x20 || c2>0x2f)))
+	if (((c1 != 0x11 && c1 != 0x19) ||
+		(c2 < 0x20 || c2 > 0x2f)))
 	{
 		ccx_common_logging.debug_ftn(CCX_DMT_DECODER_608, "\rThis is not a text attribute!\n");
 	}
@@ -312,7 +312,7 @@ int write_cc_buffer(ccx_decoder_608_context *context, struct cc_subtitle *sub)
 
 	if (!data->empty && context->output_format != CCX_OF_NULL)
 	{
-		sub->data = (struct eia608_screen *) realloc(sub->data,( sub->nb_data + 1 ) * sizeof(*data));
+		sub->data = (struct eia608_screen *) realloc(sub->data, (sub->nb_data + 1) * sizeof(*data));
 		if (!sub->data)
 		{
 			ccx_common_logging.log_ftn("No Memory left");

linux/Makefile.am
@@ -160,6 +160,7 @@ ccextractor_SOURCES = \
 				../src/lib_ccx/ccx_encoders_mcc.h \
 				../src/lib_ccx/ccx_encoders_python.c \
 				../src/lib_ccx/ccx_encoders_sami.c \
+				../src/lib_ccx/ccx_encoders_scc.c \
 				../src/lib_ccx/ccx_encoders_smptett.c \
 				../src/lib_ccx/ccx_encoders_splitbysentence.c \
 				../src/lib_ccx/ccx_encoders_spupng.c \

mac/Makefile.am
@@ -159,6 +159,7 @@ ccextractor_SOURCES = \
 				../src/lib_ccx/ccx_encoders_mcc.h \
 				../src/lib_ccx/ccx_encoders_python.c \
 				../src/lib_ccx/ccx_encoders_sami.c \
+				../src/lib_ccx/ccx_encoders_scc.c \
 				../src/lib_ccx/ccx_encoders_smptett.c \
 				../src/lib_ccx/ccx_encoders_splitbysentence.c \
 				../src/lib_ccx/ccx_encoders_spupng.c \

src/CMakeLists.txt
@@ -141,7 +141,7 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 endif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 
 
-set (EXTRA_LIBS ${EXTRA_LIBS} -lz -lm -lpthread)
+set (EXTRA_LIBS ${EXTRA_LIBS} -lm -lpthread)
 
 find_package (PkgConfig)
 

src/ccextractor.c
@@ -391,7 +391,6 @@ int api_start(struct ccx_s_options api_options)
             break;
     } // file loop
     close_input_file(ctx);
-    free((void *)ctx->extension);
 
     prepare_for_new_file(ctx); // To reset counters used by handle_end_of_data()
 

src/lib_ccx/ccx_common_common.c
@@ -51,11 +51,11 @@ void millis_to_time(LLONG milli, unsigned *hours, unsigned *minutes,
 	// LLONG milli = (LLONG) ((ccblock*1000)/29.97);
 	*ms = (unsigned)(milli % 1000); // milliseconds
 	milli = (milli - *ms) / 1000;  // Remainder, in seconds
-	*seconds = (int)(milli % 60);
+	*seconds = (int) milli % 60;
 	milli = (milli - *seconds) / 60; // Remainder, in minutes
 	*minutes = (int)(milli % 60);
 	milli = (milli - *minutes) / 60; // Remainder, in hours
-	*hours = (int)milli;
+	*hours = (int) milli;
 }
 
 /* Frees the given pointer */
@@ -101,9 +101,11 @@ int add_cc_sub_text(struct cc_subtitle *sub, char *str, LLONG start_time,
 	return 0;
 }
 
+// returns 1 if odd parity and 0 if even parity
+// Same api interface as GNU extension __builtin_parity
 int cc608_parity(unsigned int byte)
 {
-	int ones = 0;
+	unsigned int ones = 0;
 
 	for (int i = 0; i < 7; i++)
 	{
@@ -111,7 +113,7 @@ int cc608_parity(unsigned int byte)
 			ones++;
 	}
 
-	return ones & 1;
+	return ones & 1; // same as `ones % 2` for positive integers
 }
 
 void cc608_build_parity_table(int *parity_table)

src/lib_ccx/ccx_common_common.h
@@ -40,6 +40,7 @@
 #define CCX_ENOMEM  -104
 
 // Declarations
+int cc608_parity(unsigned int byte);
 void fdprintf(int fd, const char *fmt, ...);
 void millis_to_time(LLONG milli, unsigned *hours, unsigned *minutes,unsigned *seconds, unsigned *ms);
 void freep(void *arg);

src/lib_ccx/ccx_common_constants.h
@@ -167,8 +167,8 @@ enum ccx_datasource
 
 enum ccx_output_format
 {
-	CCX_OF_RAW	      = 0,
-	CCX_OF_SRT	      = 1,
+	CCX_OF_RAW        = 0,
+	CCX_OF_SRT        = 1,
 	CCX_OF_SAMI       = 2,
 	CCX_OF_TRANSCRIPT = 3,
 	CCX_OF_RCWT       = 4,
@@ -181,7 +181,9 @@ enum ccx_output_format
 	CCX_OF_G608       = 11,
 	CCX_OF_CURL       = 12,
 	CCX_OF_SSA        = 13,
-    CCX_OF_MCC        = 14
+	CCX_OF_MCC        = 14,
+	CCX_OF_SCC        = 15,
+	CCX_OF_CCD        = 16,
 };
 
 enum ccx_output_date_format

commit e3245dc3ab1501cff0ae777e3d3a8edff3795c26

[FIX] Fix tags displaying incorrectly
This was caused by 19241744d71adeda0f29c553b966236e88312c94, moving from
`unsigned char` to `enums` for colors and fonts. The problem with this is
that each colour isn't one byte next to each other so memcpy and memset
didn't work anymore.

The problem:

```patch
6812,6813c6812,6813
< EDITION OF AMERICA'S NEXT TOP
< <i> MODEL</i> ON WEDNESDAYS.<i>          </i>
---
> EDITION OF<i> AMERICA'S NEXT TOP</i>
> <i> MODEL</i> ON WEDNESDAYS.
6817c6817
< EDITION OF AMERICA'S NEXT TOP
---
> EDITION OF<i> AMERICA'S NEXT TOP</i>
6819c6819
< >><i> THE VAMPIRE DIARIES         </i>
---
> >><i> THE VAMPIRE DIARIES</i>
6824,6825c6824,6825
< >><i> THE VA</i>MPIRE DIARIES
< AND<i> THE SECRET CIRCLE          </i>
---
> >><i> THE VAMPIRE DIARIES</i>
> AND<i> THE SECRET CIRCLE</i>
6829,6831c6829,6831
< >><i> THE VA</i>MPIRE DIARIES
< AND<i> THE S</i>ECRET CIRCLE
< ON THURSDAYS.<i>                  </i>
---
> >><i> THE VAMPIRE DIARIES</i>
> AND<i> THE SECRET CIRCLE</i>
> ON THURSDAYS.
6835c6835
< AND<i> THE S</i>ECRET CIRCLE
---
> AND<i> THE SECRET CIRCLE</i>
```

src/lib_ccx/ccx_decoders_608.c
@@ -109,7 +109,7 @@ void clear_eia608_cc_buffer(ccx_decoder_608_context *context, struct eia608_scre
 		memset(data->characters[i], ' ', CCX_DECODER_608_SCREEN_WIDTH);
 		data->characters[i][CCX_DECODER_608_SCREEN_WIDTH] = 0;
 
-		for (int j = 0; j < CCX_DECODER_608_SCREEN_WIDTH + 1; j++)
+		for (int j = 0; j < CCX_DECODER_608_SCREEN_WIDTH + 1; ++j)
 		{
 			data->colors[i][j] = context->settings->default_color;
 			data->fonts[i][j] = FONT_REGULAR;
@@ -526,24 +526,39 @@ int roll_up(ccx_decoder_608_context *context)
 		if (j >= 0)
 		{
 			memcpy(use_buffer->characters[j], use_buffer->characters[j + 1], CCX_DECODER_608_SCREEN_WIDTH + 1);
-			memcpy(use_buffer->colors[j], use_buffer->colors[j + 1], CCX_DECODER_608_SCREEN_WIDTH + 1);
-			memcpy(use_buffer->fonts[j], use_buffer->fonts[j + 1], CCX_DECODER_608_SCREEN_WIDTH + 1);
-			use_buffer->row_used[j] = use_buffer->row_used[j + 1];
+
+			for (int i = 0; i < CCX_DECODER_608_SCREEN_WIDTH + 1; ++i)
+			{
+				use_buffer->colors[j][i] = use_buffer->colors[j + 1][i];
+				use_buffer->fonts[j][i] = use_buffer->fonts[j + 1][i];
+			}
+
+			use_buffer->row_used[j]=use_buffer->row_used[j+1];
 		}
 	}
 	for (int j = 0; j < (1 + context->cursor_row - keep_lines); j++)
 	{
 		memset(use_buffer->characters[j], ' ', CCX_DECODER_608_SCREEN_WIDTH);
-		memset(use_buffer->colors[j], context->settings->default_color, CCX_DECODER_608_SCREEN_WIDTH);
-		memset(use_buffer->fonts[j], FONT_REGULAR, CCX_DECODER_608_SCREEN_WIDTH);
 		use_buffer->characters[j][CCX_DECODER_608_SCREEN_WIDTH] = 0;
+
+		for (int i = 0; i < CCX_DECODER_608_SCREEN_WIDTH; ++i)
+		{
+			use_buffer->colors[j][i] = context->settings->default_color;
+			use_buffer->fonts[j][i] = FONT_REGULAR;
+		}
+
 		use_buffer->row_used[j] = 0;
 	}
-	memset(use_buffer->characters[lastrow], ' ', CCX_DECODER_608_SCREEN_WIDTH);
-	memset(use_buffer->colors[lastrow], context->settings->default_color, CCX_DECODER_608_SCREEN_WIDTH);
-	memset(use_buffer->fonts[lastrow], FONT_REGULAR, CCX_DECODER_608_SCREEN_WIDTH);
 
+	memset(use_buffer->characters[lastrow], ' ', CCX_DECODER_608_SCREEN_WIDTH);
 	use_buffer->characters[lastrow][CCX_DECODER_608_SCREEN_WIDTH] = 0;
+
+	for (int i = 0; i < CCX_DECODER_608_SCREEN_WIDTH; ++i)
+	{
+		use_buffer->colors[lastrow][i] = context->settings->default_color;
+		use_buffer->fonts[lastrow][i] = FONT_REGULAR;
+	}
+
 	use_buffer->row_used[lastrow] = 0;
 
 	// Sanity check
@@ -949,9 +964,13 @@ void handle_pac(unsigned char c1, unsigned char c2, ccx_decoder_608_context *con
 			if (use_buffer->row_used[j])
 			{
 				memset(use_buffer->characters[j], ' ', CCX_DECODER_608_SCREEN_WIDTH);
-				memset(use_buffer->colors[j], context->settings->default_color, CCX_DECODER_608_SCREEN_WIDTH);
-				memset(use_buffer->fonts[j], FONT_REGULAR, CCX_DECODER_608_SCREEN_WIDTH);
 				use_buffer->characters[j][CCX_DECODER_608_SCREEN_WIDTH] = 0;
+
+				for (int i = 0; i < CCX_DECODER_608_SCREEN_WIDTH; ++i)
+				{
+					use_buffer->colors[j][i] = context->settings->default_color;
+					use_buffer->fonts[j][i] = FONT_REGULAR;
+				}
 				use_buffer->row_used[j] = 0;
 			}
 		}