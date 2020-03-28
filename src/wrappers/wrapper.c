289e4f8205535f89251ea65b221580f794632ab8
canihavesomecoffee <git@canihavesome.coffee>
2020-03-28 23:10:34 +0100

    Apply clang-format to all wrappers files

--- src/wrappers/wrapper.c
+++ src/wrappers/wrapper.c
@@ -2,35 +2,40 @@
 #include "ccextractor.h"
 
 #ifdef PYTHON_API
-void my_pythonapi(struct ccx_s_options *api_options, PyObject *callback_func) {
+void my_pythonapi(struct ccx_s_options *api_options, PyObject *callback_func)
+{
 	py_callback_func = callback_func;
 }
 
 /*
 autoprogram wrapper
 */
-void setautoprogram(struct ccx_s_options *api_options) {
+void setautoprogram(struct ccx_s_options *api_options)
+{
 	api_add_param(api_options, "-autoprogram");
 }
 
 /*
 stdout wrapper
 */
-void setstdout(struct ccx_s_options *api_options) {
+void setstdout(struct ccx_s_options *api_options)
+{
 	api_add_param(api_options, "-stdout");
 }
 
 /*
 pesheader wrapper
 */
-void setpesheader(struct ccx_s_options *api_options) {
+void setpesheader(struct ccx_s_options *api_options)
+{
 	api_add_param(api_options, "-pesheader");
 }
 
 /*
 debugdvbsub wrapper
 */
-void setdebugdvbsub(struct ccx_s_options *api_options) {
+void setdebugdvbsub(struct ccx_s_options *api_options)
+{
 	api_add_param(api_options, "-debugdvbsub");
 }
 #endif
