cf84757e02889ffa0231624fdf941eafded59906
MackeyStingray <45679755+MackeyStingray@users.noreply.github.com>
2020-09-13 10:10:02 -0700

    Fix hardsubx segmentation fault (#1280)

--- src/lib_ccx/ocr.h
+++ src/lib_ccx/ocr.h
@@ -13,6 +13,7 @@ struct image_copy //A copy of the original OCR image, used for color detection
 };
 
 void delete_ocr (void** arg);
+char* probe_tessdata_location(const char *lang);
 void* init_ocr(int lang_index);
 char* ocr_bitmap(void* arg, png_color *palette,png_byte *alpha, unsigned char* indata,int w, int h, struct image_copy *copy);
 int ocr_rect(void* arg, struct cc_bitmap *rect, char **str, int bgcolor, int ocr_quantmode);
