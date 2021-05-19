CCExtractor/ccextractor

commit 403581462e8beb171b105b82ab8b60ace96b1695

[FIX] Remove webvtt styling when not using webvtt-full (#1092)
* no styling unless in full mode

* part 1 of moving style to here

* no style header unless requested with webvtt-full

* only one new line to support x-timestamp-map

* move x-timestamp-map up to abide by specifications

and support ffmpeg and brightcove

* remove stray new line, crlfs are added upstream

297 seems to contain a null bug

* don't write null characters to sub file

* needed space after -full mode style

* typo

src/lib_ccx/ccx_encoders_common.c
@@ -93,7 +93,7 @@ static const char *smptett_header = "<?xml version=\"1.0\" encoding=\"UTF-8\" st
 			"  <body>\n"
 			"    <div>\n";
 
-static const char *webvtt_header[] = {"WEBVTT","\r\n","\r\n","STYLE","\r\n","\r\n",NULL};
+static const char *webvtt_header[] = {"WEBVTT","\r\n",NULL};
 
 static const char *simple_xml_header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<captions>\r\n";
 

src/lib_ccx/ccx_encoders_webvtt.c
@@ -8,7 +8,8 @@
 
 static const char *webvtt_outline_css = "@import(%s)\n";
 
-static const char *webvtt_inline_css = "/* default values */\n"
+static const char *webvtt_inline_css = "\r\nSTYLE\n\n"
+		"/* default values */\n"
 		"::cue {\n"
 		"  line-height: 5.33vh;\n"
 		"  font-size: 4.1vh;\n"
@@ -207,6 +208,20 @@ int write_webvtt_header(struct encoder_ctx *context)
 	if (context->wrote_webvtt_header) // Already done
 		return 1;
 
+	if (context->timing->sync_pts2fts_set)
+	{
+		char header_string[200];
+		int used;
+		unsigned h1, m1, s1, ms1;
+		millis_to_time(context->timing->sync_pts2fts_fts, &h1, &m1, &s1, &ms1);
+		sprintf(header_string, "X-TIMESTAMP-MAP=MPEGTS:%ld, LOCAL %02u:%02u:%02u.%03u\r\n\n",
+			context->timing->sync_pts2fts_pts,
+			h1, m1, s1, ms1);
+		used = encode_line(context, context->buffer, (unsigned char *)header_string);
+		write(context->out->fh, context->buffer, used);
+
+	}
+	
 	if (ccx_options.webvtt_create_css)
 	{
 		char *basefilename = get_basename(context->first_input_file);
@@ -225,7 +240,7 @@ int write_webvtt_header(struct encoder_ctx *context)
 		char* outline_css_file = (char*)malloc((strlen(css_file_name) + strlen(webvtt_outline_css)) * sizeof(char));
 		sprintf(outline_css_file, webvtt_outline_css, css_file_name);
 		write (context->out->fh, outline_css_file, strlen(outline_css_file));
-	} else {
+	} else if (ccx_options.use_webvtt_styling) {
 		write(context->out->fh, webvtt_inline_css, strlen(webvtt_inline_css));
 		if(ccx_options.enc_cfg.line_terminator_lf == 1) // If -lf parameter is set.
 		{
@@ -235,23 +250,8 @@ int write_webvtt_header(struct encoder_ctx *context)
 		{
 			write(context->out->fh,"\r\n",2);
 		}
-	}
-
-	write(context->out->fh, "##\n", 3);
-	write(context->out->fh, context->encoded_crlf, context->encoded_crlf_length);
-
-	if (context->timing->sync_pts2fts_set)
-	{
-		char header_string[200];
-		int used;
-		unsigned h1, m1, s1, ms1;
-		millis_to_time(context->timing->sync_pts2fts_fts, &h1, &m1, &s1, &ms1);
-		sprintf(header_string, "X-TIMESTAMP-MAP=MPEGTS:%ld, LOCAL %02u:%02u:%02u.%03u\r\n\n",
-			context->timing->sync_pts2fts_pts,
-			h1, m1, s1, ms1);
-		used = encode_line(context, context->buffer, (unsigned char *)header_string);
-		write(context->out->fh, context->buffer, used);
-
+		write(context->out->fh, "##\n", 3);
+		write(context->out->fh, context->encoded_crlf, context->encoded_crlf_length);
 	}
 
 	context->wrote_webvtt_header = 1; // Do it even if couldn't write the header, because it won't be possible anyway
@@ -294,7 +294,7 @@ int write_cc_bitmap_as_webvtt(struct cc_subtitle *sub, struct encoder_ctx *conte
 			sprintf(timeline, "%02u:%02u:%02u.%03u --> %02u:%02u:%02u.%03u%s",
 				h1, m1, s1, ms1, h2, m2, s2, ms2, context->encoded_crlf);
 			used = encode_line(context, context->buffer, (unsigned char *)timeline);
-            write(context->out->fh, context->buffer, used);
+			write(context->out->fh, context->buffer, used);
 			len = strlen(str);
 			write(context->out->fh, str, len);
 			write(context->out->fh, context->encoded_crlf, context->encoded_crlf_length);
@@ -492,7 +492,8 @@ int write_cc_buffer_as_webvtt(struct eia608_screen *data, struct encoder_ctx *co
 				}
 
 				// write current text symbol
-				write(context->out->fh, &(context->subline[j]), 1);
+				if (context->subline[j] != '\0')
+					write(context->out->fh, &(context->subline[j]), 1);
 
 				if (ccx_options.use_webvtt_styling)
 				{

commit 5b29ef281a02319e3597dc1bbe3f3ac18fbaa500

[FIX] Multitrack, WebVTT, and Segfault issues (#1332)
* [FIX] Must have two newlines after WEBVTT header

Bug introduced in #1092

* [FIX] segfault with multitrack reports

* [FIX] segfault with unsupported file reports

* [FIX] Write subtitle header to multitrack outputs

* [FIX] Write multitrack files to the output file directory

src/lib_ccx/ccx_encoders_common.c
@@ -92,7 +92,7 @@ static const char *smptett_header = "<?xml version=\"1.0\" encoding=\"UTF-8\" st
 				    "  <body>\n"
 				    "    <div>\n";
 
-static const char *webvtt_header[] = {"WEBVTT", "\r\n", NULL};
+static const char *webvtt_header[] = {"WEBVTT", "\r\n", "\r\n", NULL};
 
 static const char *simple_xml_header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<captions>\r\n";
 
@@ -1448,6 +1448,12 @@ unsigned int get_font_encoded(struct encoder_ctx *ctx, unsigned char *buffer, in
 
 void switch_output_file(struct lib_ccx_ctx *ctx, struct encoder_ctx *enc_ctx, int track_id)
 {
+	// Do nothing when in "report" mode
+	if (enc_ctx == NULL || enc_ctx->out == NULL)
+	{
+		return;
+	}
+
 	if (enc_ctx->out->filename != NULL)
 	{ // Close and release the previous handle
 		free(enc_ctx->out->filename);
@@ -1456,8 +1462,15 @@ void switch_output_file(struct lib_ccx_ctx *ctx, struct encoder_ctx *enc_ctx, in
 	const char *ext = get_file_extension(ctx->write_format);
 	char suffix[32];
 	sprintf(suffix, "_%d", track_id);
-	enc_ctx->out->filename = create_outfilename(get_basename(enc_ctx->first_input_file), suffix, ext);
-	enc_ctx->out->fh = open(enc_ctx->out->filename, O_RDWR | O_CREAT | O_TRUNC | O_BINARY, S_IREAD | S_IWRITE);
+	char *basename = get_basename(enc_ctx->out->original_filename);
+	if (basename != NULL)
+	{
+		enc_ctx->out->filename = create_outfilename(basename, suffix, ext);
+		enc_ctx->out->fh = open(enc_ctx->out->filename, O_RDWR | O_CREAT | O_TRUNC | O_BINARY, S_IREAD | S_IWRITE);
+		free(basename);
+	}
+
+	write_subtitle_file_header(enc_ctx, enc_ctx->out);
 
 	// Reset counters as we switch output file.
 	enc_ctx->cea_708_counter = 0;

src/lib_ccx/ccx_encoders_structs.h
@@ -18,6 +18,7 @@ struct ccx_s_write
 	int fh;
 	int temporarily_closed; // 1 means the file was created OK before but we released the handle
 	char *filename;
+	char *original_filename;
 	void* spupng_data;
 	int with_semaphore;     // 1 means create a .sem file when the file is open and delete it when it's closed
 	char *semaphore_filename;

src/lib_ccx/output.c
@@ -12,6 +12,7 @@ void dinit_write(struct ccx_s_write *wb)
 	if (wb->fh > 0)
 		close(wb->fh);
 	freep(&wb->filename);
+	freep(&wb->original_filename);
 	if (wb->with_semaphore && wb->semaphore_filename)
 		unlink(wb->semaphore_filename);
 	freep(&wb->semaphore_filename);
@@ -50,6 +51,7 @@ int init_write(struct ccx_s_write *wb, char *filename, int with_semaphore)
 	wb->fh = -1;
 	wb->temporarily_closed = 0;
 	wb->filename = filename;
+	wb->original_filename = strdup(filename);
 
 	wb->with_semaphore = with_semaphore;
 	wb->append_mode = ccx_options.enc_cfg.append_mode;

src/lib_ccx/params_dump.c
@@ -269,6 +269,11 @@ void print_file_report(struct lib_ccx_ctx *ctx)
 				return;
 			}
 
+			if (ctx->current_file >= ctx->num_input_files)
+			{
+				return;
+			}
+
 			printf("%s\n", ctx->inputfile[ctx->current_file]);
 			break;
 		case CCX_DS_STDIN: