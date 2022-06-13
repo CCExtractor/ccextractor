d0d849a2ab1f769641900028a2a9cadd9d377067
Shashwat Singh <singhshashwat1002@gmail.com>
2022-06-13 23:22:40 +0530

    replace destructuring assignment statement with normal assignment statements because of build rust compiler issues

--- src/rust/src/hardsubx/imgops.rs
+++ src/rust/src/hardsubx/imgops.rs
@@ -6,11 +6,9 @@ pub extern "C" fn rgb_to_hsv(R: f32, G: f32, B: f32, H: &mut f32, S: &mut f32, V
 
     let hsv_rep = Hsv::from_color(rgb);
 
-    (*H, *S, *V) = (
-        hsv_rep.hue.to_positive_degrees(),
-        hsv_rep.saturation,
-        hsv_rep.value,
-    );
+    *H = hsv_rep.hue.to_positive_degrees();
+    *S = hsv_rep.saturation;
+    *V = hsv_rep.value;
 }
 
 #[no_mangle]
@@ -20,5 +18,7 @@ pub extern "C" fn rgb_to_lab(R: f32, G: f32, B: f32, L: &mut f32, a: &mut f32, b
     let lab_rep = Lab::<palette::white_point::D65, f32>::from_color(rgb);
     // The above declaration sets the white-point as per the D65 standard
 
-    (*L, *a, *b) = (lab_rep.l, lab_rep.a, lab_rep.b);
+    *L = lab_rep.l;
+    *a = lab_rep.a;
+    *b = lab_rep.b;
 }
