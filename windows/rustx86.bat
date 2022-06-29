12c09e6b15653063a2bcfe2c20d68dad3e0522ef
Shashwat Singh <singhshashwat1002@gmail.com>
2022-06-29 22:40:46 +0530

    enable hardsubx_ocr feature in windows build

--- windows/rustx86.bat
+++ windows/rustx86.bat
@@ -1,6 +1,6 @@
 for /f "delims=" %%i in ('cd') do set output=%%i
 set CARGO_TARGET_DIR=%output%
 cd ..\src\rust
-cargo build --target=i686-pc-windows-msvc
+cargo build --target=i686-pc-windows-msvc --features "hardsubx_ocr"
 cd ..\..\windows
 copy i686-pc-windows-msvc\debug\ccx_rust.lib .\ccx_rust.lib
