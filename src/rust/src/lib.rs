683b651912f8b3ff77044cf56dfad4fd4a4c9e34
Shashwat Singh <singhshashwat1002@gmail.com>
2022-07-03 00:57:32 +0530

    style changes

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -12,11 +12,9 @@
 pub mod bindings {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
-pub mod decoder;
-
 #[cfg(feature = "hardsubx_ocr")]
 pub mod hardsubx;
-
+pub mod decoder;
 pub mod utils;
 
 #[cfg(windows)]
