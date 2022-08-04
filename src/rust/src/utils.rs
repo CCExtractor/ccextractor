334e216833784e2ab3f0245297a1acfc5e08ec79
Shashwat Singh <singhshashwat1002@gmail.com>
2022-08-04 19:31:33 +0530

    - write a function to convert rust strings to c strings - write a memory safe wrapper to mprint that uses above function

--- src/rust/src/utils.rs
+++ src/rust/src/utils.rs
@@ -1,4 +1,6 @@
 //! Some utility functions to deal with values from C bindings
+use crate::bindings::mprint;
+use std::ffi;
 
 /// Check if the value is true (Set to 1). Use only for values from C bindings
 pub fn is_true<T: Into<i32>>(val: T) -> bool {
@@ -8,4 +10,21 @@ pub fn is_true<T: Into<i32>>(val: T) -> bool {
 /// Check if the value is false (Set to 0). Use only for values from C bindings
 pub fn is_false<T: Into<i32>>(val: T) -> bool {
     val.into() == 0
+}
+
+/// function to convert Rust literals to C strings to be passed into functions
+pub unsafe fn string_to_c_char(a: &str) -> *mut ::std::os::raw::c_char {
+    let s = ffi::CString::new(a).unwrap();
+
+    s.into_raw()
+}
+
+/// function to call mprint without memory leak
+pub unsafe fn mprint_mem_safe(a: &str) {
+    let mprint_str = string_to_c_char(a);
+
+    mprint(mprint_str);
+
+    // to deallocate memory properly
+    let _S = ffi::CString::from_raw(mprint_str);
 }
