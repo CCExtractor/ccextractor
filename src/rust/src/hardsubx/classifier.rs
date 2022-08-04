3d57f860a81aa91ead154d8184666e58059180e1
Shashwat Singh <singhshashwat1002@gmail.com>
2022-08-04 19:33:30 +0530

    - add helper function to deal with tess strings in a memory safe manner - port get_ocr_text_simple - port get_ocr_text_wordwise

--- src/rust/src/hardsubx/classifier.rs
+++ src/rust/src/hardsubx/classifier.rs
@@ -7,10 +7,17 @@ use tesseract_sys::*;
 #[cfg(feature = "hardsubx_ocr")]
 use leptonica_sys::*;
 
+use std::ffi;
+use std::ptr::null;
+
 pub type ccx_output_format = ::std::os::raw::c_uint;
 pub type subdatatype = ::std::os::raw::c_uint;
 pub type subtype = ::std::os::raw::c_uint;
 pub type ccx_encoding_type = ::std::os::raw::c_uint;
+
+use crate::utils::{mprint_mem_safe, string_to_c_char};
+
+use std::os::raw::c_char;
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
@@ -76,4 +83,120 @@ pub struct lib_hardsubx_ctx {
     pub conf_thresh: f32,
     pub hue: f32,
     pub lum_thresh: f32,
+}
+
+pub unsafe extern "C" fn get_ocr_text_simple(
+    ctx: *mut lib_hardsubx_ctx,
+    image: *mut PIX,
+) -> *mut ::std::os::raw::c_char {
+    let text_out: *mut ::std::os::raw::c_char;
+
+    TessBaseAPISetImage2((*ctx).tess_handle, image);
+
+    if TessBaseAPIRecognize((*ctx).tess_handle, null::<ETEXT_DESC>() as *mut ETEXT_DESC) != 0 {
+        mprint_mem_safe("Error in Tesseract recognition, skipping frame\n");
+        null::<c_char>() as *mut c_char
+    } else {
+        text_out = TessBaseAPIGetUTF8Text((*ctx).tess_handle);
+
+        if text_out == null::<c_char>() as *mut c_char {
+            mprint_mem_safe("Error getting text, skipping frame\n");
+        }
+        text_out
+    }
+}
+
+unsafe fn _tess_string_helper(it: *mut TessResultIterator, level: TessPageIteratorLevel) -> String {
+    // Function extracts string from tess iterator object
+    // frees memory associated with tesseract string
+    // takes and gives ownership of the string
+
+    let ts_word_ptr: *mut ::std::os::raw::c_char = TessResultIteratorGetUTF8Text(it, level);
+
+    if ts_word_ptr == null::<c_char>() as *mut c_char {
+        // this is required because trying to generate
+        // CStr from null pointer will be a segmentation fault
+        return String::new();
+    }
+
+    let ts_word = ffi::CStr::from_ptr(ts_word_ptr);
+    let ts_word_arr = ffi::CStr::to_bytes_with_nul(&ts_word);
+
+    let ts_word_string: String = match String::from_utf8(ts_word_arr.to_vec()) {
+        Ok(string_rep) => string_rep,
+        Err(error) => std::panic::panic_any(error),
+    };
+
+    TessDeleteText(ts_word_ptr);
+    // clean up the memory
+
+    ts_word_string
+}
+
+pub unsafe extern "C" fn get_ocr_text_wordwise(
+    ctx: *mut lib_hardsubx_ctx,
+    image: *mut PIX,
+) -> *mut ::std::os::raw::c_char {
+    let mut text_out = String::new();
+
+    TessBaseAPISetImage2((*ctx).tess_handle, image);
+
+    if TessBaseAPIRecognize((*ctx).tess_handle, null::<ETEXT_DESC>() as *mut ETEXT_DESC) != 0 {
+        mprint_mem_safe("Error in Tesseract recognition, skipping word\n");
+        return null::<c_char>() as *mut c_char;
+    }
+
+    let it: *mut TessResultIterator = TessBaseAPIGetIterator((*ctx).tess_handle);
+    let level: TessPageIteratorLevel = TessPageIteratorLevel_RIL_WORD;
+
+    let mut prev_ital: bool = false;
+
+    if it != null::<TessResultIterator>() as *mut TessResultIterator {
+        loop {
+            let mut word = _tess_string_helper(it, level);
+
+            if word.len() == 0 {
+                continue;
+            }
+
+            if (*ctx).detect_italics != 0 {
+                let mut italic: i32 = 0;
+                let mut dummy: i32 = 0;
+
+                TessResultIteratorWordFontAttributes(
+                    it,
+                    &mut dummy as *mut i32,
+                    &mut italic as *mut i32,
+                    &mut dummy as *mut i32,
+                    &mut dummy as *mut i32,
+                    &mut dummy as *mut i32,
+                    &mut dummy as *mut i32,
+                    &mut dummy as *mut i32,
+                    &mut dummy as *mut i32,
+                );
+                if italic != 0 && !prev_ital {
+                    word = format!("<i>{}", word);
+                    prev_ital = true;
+                } else if italic == 0 && prev_ital {
+                    word = format!("{}</i>", word);
+                    prev_ital = false;
+                }
+            }
+
+            text_out = format!("{} {}", text_out, word);
+
+            if TessPageIteratorNext(it as *mut TessPageIterator, level) == 0 {
+                if (*ctx).detect_italics == 1 && prev_ital {
+                    // if there are italics words at the end
+                    text_out = format!("{}</i>", text_out);
+                }
+                break;
+            }
+        }
+    }
+
+    TessResultIteratorDelete(it);
+
+    // TODO: this is a memory leak that can only be plugged when the caller functions have been ported
+    return string_to_c_char(&text_out);
 }
