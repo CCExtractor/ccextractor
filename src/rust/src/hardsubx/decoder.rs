b0d3888792a90c929d8ec91a967f10baa0808023
Shashwat Singh <singhshashwat1002@gmail.com>
2022-11-17 18:14:20 +0530

    port hardsubx_process_frames_linear function to the decoder

--- src/rust/src/hardsubx/decoder.rs
+++ src/rust/src/hardsubx/decoder.rs
@@ -1,4 +1,6 @@
 #[cfg(feature = "hardsubx_ocr")]
+use ffmpeg_sys_next::*;
+#[cfg(feature = "hardsubx_ocr")]
 use leptonica_sys::*;
 
 // #[cfg(feature = "hardsubx_ocr")]
@@ -8,24 +10,26 @@ use std::convert::TryInto;
 use std::eprintln;
 use std::ffi;
 use std::format;
+use std::io::empty;
 use std::os::raw::c_char;
 use std::process::exit;
 use std::ptr::null;
 
+use crate::bindings::{activity_progress, add_cc_sub_text, cc_subtitle, encode_sub, encoder_ctx};
 #[cfg(feature = "hardsubx_ocr")]
 // use crate::bindings::{hardsubx_ocr_mode_HARDSUBX_OCRMODE_WORD};
-use crate::bindings::AVFrame;
 use crate::hardsubx::classifier::*;
 use crate::hardsubx::imgops::{rgb_to_hsv, rgb_to_lab};
 use crate::hardsubx::lib_hardsubx_ctx;
 use crate::utils::string_to_c_char;
 
-use super::AVPacket;
-use super::HardsubxContext;
+use super::hardsubx_color_type;
 use super::hardsubx_ocr_mode;
-use super::utility::convert_pts_to_ms;
+use super::utility::*;
+use super::HardsubxContext;
+use super::CCX_ENC_UTF_8;
 
-use std::num;
+use std::{cmp, num};
 
 static EXIT_MALFORMED_PARAMETER: i32 = 7;
 
@@ -61,7 +65,7 @@ pub unsafe fn dispatch_classifier_functions(ctx: &mut HardsubxContext, im: *mut 
         _ => {
             eprintln!("Invalid OCR Mode");
             exit(EXIT_MALFORMED_PARAMETER);
-            // "".to_string()
+            // String::new()
         }
     }
 }
@@ -326,4 +330,239 @@ pub unsafe extern "C" fn _process_frame_tickertext(
     pixDestroy(&mut feat_im as *mut *mut Pix);
 
     subtitle_text
+}
+
+pub unsafe fn hardsubx_process_frames_linear(ctx: &mut HardsubxContext, enc_ctx: *mut encoder_ctx) {
+    let mut prev_sub_encoded: bool = true;
+    let mut got_frame = 0;
+    let mut dist = 0;
+    let mut cur_sec = 0;
+    let mut total_sec = 0;
+    let mut progress = 0;
+
+    let mut frame_number = 0;
+
+    let mut prev_begin_time: i64 = 0;
+    let mut prev_end_time: i64 = 0;
+
+    let mut prev_packet_pts: i64 = 0;
+
+    let mut subtitle_text: String = String::new();
+    let mut prev_subtitle_text: String = String::new();
+
+    while av_read_frame(ctx.format_ctx, &mut ctx.packet as *mut AVPacket) >= 0 {
+        if (ctx.packet.stream_index == ctx.video_stream_id) {
+            frame_number += 1;
+
+            let mut status = avcodec_send_packet(ctx.codec_ctx, &mut ctx.packet as *mut AVPacket);
+            // status = avcodec_receive_frame(ctx.codec_ctx, ctx.frame);
+
+            if status >= 0 || status == AVERROR(EAGAIN) || status == AVERROR_EOF {
+                if status >= 0 {
+                    ctx.packet.size = 0;
+                }
+
+                status = avcodec_receive_frame(ctx.codec_ctx, ctx.frame);
+
+                if status == 0 {
+                    got_frame = 1;
+                }
+            }
+
+            if got_frame != 0 && frame_number % 25 == 0 {
+                let diff = convert_pts_to_ms(
+                    ctx.packet.pts - prev_packet_pts,
+                    (**(*ctx.format_ctx)
+                        .streams
+                        .offset(ctx.video_stream_id.try_into().unwrap()))
+                    .time_base,
+                );
+
+                if (diff.abs() as f32) < 1000.0 * ctx.min_sub_duration {
+                    continue;
+                }
+
+                sws_scale(
+                    ctx.sws_ctx,
+                    (*ctx.frame).data.as_ptr() as *const *const u8,
+                    (*ctx.frame).linesize.as_mut_ptr(),
+                    0,
+                    (*ctx.codec_ctx).height,
+                    (*ctx.rgb_frame).data.as_mut_ptr(),
+                    (*ctx.rgb_frame).linesize.as_mut_ptr(),
+                );
+
+                subtitle_text = match ctx.subcolor {
+                    hardsubx_color_type::HARDSUBX_COLOR_WHITE => _process_frame_white_basic(
+                        ctx,
+                        ctx.rgb_frame,
+                        (*ctx.codec_ctx).width,
+                        (*ctx.codec_ctx).height,
+                        frame_number,
+                    ),
+                    _ => _process_frame_color_basic(
+                        ctx,
+                        ctx.rgb_frame,
+                        (*ctx.codec_ctx).width,
+                        (*ctx.codec_ctx).height,
+                        frame_number,
+                    ),
+                };
+
+                cur_sec = convert_pts_to_s(
+                    ctx.packet.pts,
+                    (**(*ctx.format_ctx)
+                        .streams
+                        .offset(ctx.video_stream_id.try_into().unwrap()))
+                    .time_base,
+                );
+                total_sec = convert_pts_to_s((*ctx.format_ctx).duration, AV_TIME_BASE_Q);
+
+                progress = (cur_sec * 100) / total_sec;
+                activity_progress(
+                    progress.try_into().unwrap(),
+                    (cur_sec / 60).try_into().unwrap(),
+                    (cur_sec % 60).try_into().unwrap(),
+                );
+
+                if subtitle_text.is_empty() && prev_subtitle_text.is_empty() {
+                    prev_end_time = convert_pts_to_ms(
+                        ctx.packet.pts,
+                        (**(*ctx.format_ctx)
+                            .streams
+                            .offset(ctx.video_stream_id.try_into().unwrap()))
+                        .time_base,
+                    );
+                }
+
+                if !subtitle_text.is_empty() {
+                    let double_enter = subtitle_text.find("\n\n");
+                    match double_enter {
+                        Some(T) => {
+                            subtitle_text = subtitle_text[0..T].to_string();
+                        }
+                        _ => {}
+                    }
+                }
+
+                if !prev_sub_encoded && !prev_subtitle_text.is_empty() {
+                    if !subtitle_text.is_empty() {
+                        dist = edit_distance_string(&subtitle_text, &prev_subtitle_text);
+                        if (dist as f32)
+                            < 0.2
+                                * (cmp::min(
+                                    subtitle_text.chars().count(),
+                                    prev_subtitle_text.chars().count(),
+                                ) as f32)
+                        {
+                            dist = -1;
+                            subtitle_text = String::new();
+                            prev_end_time = convert_pts_to_ms(
+                                ctx.packet.pts,
+                                (**(*ctx.format_ctx)
+                                    .streams
+                                    .offset(ctx.video_stream_id.try_into().unwrap()))
+                                .time_base,
+                            );
+                        }
+                    }
+
+                    if dist != -1 {
+                        let sub_text_chr = string_to_c_char(&subtitle_text);
+                        let prev_text_chr = string_to_c_char(&prev_subtitle_text);
+                        let empty_chr = string_to_c_char("");
+                        let mode_chr = string_to_c_char("BURN");
+                        add_cc_sub_text(
+                            &mut *ctx.dec_sub as *mut cc_subtitle,
+                            prev_text_chr,
+                            prev_begin_time,
+                            prev_begin_time,
+                            empty_chr,
+                            mode_chr,
+                            CCX_ENC_UTF_8,
+                        );
+
+                        encode_sub(enc_ctx, &mut *ctx.dec_sub);
+
+                        // Deallocation
+                        subtitle_text = ffi::CString::from_raw(sub_text_chr)
+                            .to_string_lossy()
+                            .into_owned();
+                        prev_subtitle_text = ffi::CString::from_raw(prev_text_chr)
+                            .to_string_lossy()
+                            .into_owned();
+                        ffi::CString::from_raw(empty_chr);
+                        ffi::CString::from_raw(mode_chr);
+
+                        prev_begin_time = prev_end_time + 1;
+                        prev_subtitle_text = String::new();
+                        prev_sub_encoded = true;
+                        prev_end_time = convert_pts_to_ms(
+                            ctx.packet.pts,
+                            (**(*ctx.format_ctx)
+                                .streams
+                                .offset(ctx.video_stream_id.try_into().unwrap()))
+                            .time_base,
+                        );
+
+                        if !subtitle_text.is_empty() {
+                            prev_subtitle_text = subtitle_text.clone();
+                            prev_sub_encoded = false;
+                        }
+                    }
+                    dist = 0;
+                }
+
+                if prev_subtitle_text.is_empty() && !subtitle_text.is_empty() {
+                    prev_begin_time = prev_end_time + 1;
+                    prev_end_time = convert_pts_to_ms(
+                        ctx.packet.pts,
+                        (**(*ctx.format_ctx)
+                            .streams
+                            .offset(ctx.video_stream_id.try_into().unwrap()))
+                        .time_base,
+                    );
+                    prev_subtitle_text = subtitle_text.clone();
+                    prev_sub_encoded = false;
+                }
+                prev_packet_pts = ctx.packet.pts;
+            }
+        }
+        av_packet_unref(&mut ctx.packet as *mut AVPacket);
+    }
+
+    if !prev_sub_encoded {
+        let sub_text_chr = string_to_c_char(&subtitle_text);
+        let prev_text_chr = string_to_c_char(&prev_subtitle_text);
+        let empty_chr = string_to_c_char("");
+        let mode_chr = string_to_c_char("BURN");
+        add_cc_sub_text(
+            &mut *ctx.dec_sub as *mut cc_subtitle,
+            prev_text_chr,
+            prev_begin_time,
+            prev_begin_time,
+            empty_chr,
+            mode_chr,
+            CCX_ENC_UTF_8,
+        );
+
+        // Deallocation
+        subtitle_text = ffi::CString::from_raw(sub_text_chr)
+            .to_string_lossy()
+            .into_owned();
+        prev_subtitle_text = ffi::CString::from_raw(prev_text_chr)
+            .to_string_lossy()
+            .into_owned();
+        ffi::CString::from_raw(empty_chr);
+        ffi::CString::from_raw(mode_chr);
+
+        encode_sub(enc_ctx, &mut *ctx.dec_sub as *mut cc_subtitle);
+        prev_sub_encoded = true;
+    }
+
+    activity_progress(
+        100,
+        (cur_sec / 60).try_into().unwrap(),
+        (cur_sec % 60).try_into().unwrap(),
+    );
 }
