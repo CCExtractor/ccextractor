93df0e6d454cc6945b02d25d09f47811d080b6d6
Sachin Rao <biz.sachinrao@gmail.com>
2023-03-17 20:55:58 +0530

    Only error left to fix

--- src/thirdparty/gpacmp4/box_dump.c
+++ src/thirdparty/gpacmp4/box_dump.c
@@ -29,8 +29,7 @@
 #include <gpac/color.h>
 #include <gpac/avparse.h>
 #include <gpac/base_coding.h>
-#include <gpac/webvtt.h>
-// #include <gpac/webvtt.c>
+#include <gpac/webvtt.c>
 
 
 #ifndef GPAC_DISABLE_ISOM_DUMP
@@ -3970,7 +3969,7 @@ static GF_Err gf_isom_dump_srt_track(GF_ISOFile *the_file, u32 track, FILE *dump
 			end_ts = end * 1000;
 			end_ts /= trak->Media->mediaHeader->timeScale;
 
-			// Getting Undefined Reference Even though webvtt.c file is included
+			//  Getting Undefined Reference Even though webvtt.c file is included
 			// cues = gf_webvtt_parse_cues_from_data(s->data, s->dataLength, start_ts, end_ts);
 			
 			gf_isom_sample_del(&s);
