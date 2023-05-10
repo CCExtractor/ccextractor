85d4772e327038dd39bcbe5639d535c0192abdb6
canihavesomecoffee <git@canihavesome.coffee>
2023-05-10 21:27:07 +0200

    Remove Nuklear GUI

--- src/GUI/activity.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACTIVITY_H
-#define ACTIVITY_H
-
-#include "ccextractorGUI.h"
-
-int activity(struct nk_context *ctx, int x, int y, int width, int height, struct main_tab *main_settings);
-
-#endif
--- windows/post-build.bat
+++
@@ -1,2 +0,0 @@
-@echo OFF
-xcopy libs\lib\pthreadVSE2.dll %1
