6bcc53ecf93c27bede15f291ba0e2f963792b80a
Elbert Ronnie <103196773+elbertronnie@users.noreply.github.com>
2023-05-21 21:05:45 +0530

    Provide unique values to enums (#1538)

--- src/lib_ccx/ccx_common_constants.h
+++ src/lib_ccx/ccx_common_constants.h
@@ -57,10 +57,10 @@ enum ccx_debug_message_types
 	CCX_DMT_PMT = 0x800,             // Program Map Table dump
 	CCX_DMT_LEVENSHTEIN = 0x1000,    // Levenshtein distance calculations
 	CCX_DMT_DVB = 0x2000,			 // DVB 
-#ifdef ENABLE_SHARING
-	CCX_DMT_SHARE = 0x2000,          // Extracted captions sharing service
-#endif //ENABLE_SHARING
 	CCX_DMT_DUMPDEF = 0x4000         // Dump defective TS packets
+#ifdef ENABLE_SHARING
+	CCX_DMT_SHARE = 0x8000,          // Extracted captions sharing service
+#endif //ENABLE_SHARING
 };
 
 // AVC NAL types
