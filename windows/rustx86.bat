9d478c802c44ff15d6ffbf87dbc72494730b6b88
canihavesomecoffee <git@canihavesome.coffee>
2023-05-22 20:58:48 +0200

    Delete rustx86.bat

--- windows/rustx86.bat
+++
@@ -1,6 +0,0 @@
-for /f "delims=" %%i in ('cd') do set output=%%i
-set CARGO_TARGET_DIR=%output%
-cd ..\src\rust
-cargo build --features "hardsubx_ocr" --target x86_64-pc-windows-msvc
-cd ..\..\windows
-copy x86_64-pc-windows-msvc\debug\ccx_rust.lib .\ccx_rust.lib
