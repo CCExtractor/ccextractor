5b7666965fa92735eba8953d8acda7ba4f7ef220
Willem <github@canihavesome.coffee>
2023-05-29 20:34:15 +0200

    Cleanup vs configs (#1539)
    * Delete (probably) wrongly committed vs config file
    
    * Remove Nuklear GUI
    
    * Clean up SLN configs (Reduce to 64 bit full debug & release)
    
    * Sync bat scripts, prepare to move
    
    * Build rust in release when release
    
    * Update changelog
    
    * Delete rustx86.bat

--- src/GUI/preview.h
+++
@@ -1,8 +0,0 @@
-#ifndef PREVIEW_H
-#define PREVIEW_H
-
-#include "ccextractorGUI.h"
-
-int preview(struct nk_context *ctx, int x, int y, int width, int height, struct main_tab *main_settings);
-
-#endif //!PREVIEW_H
--- windows/rust.bat
+++ windows/rust.bat
@@ -1,6 +1,10 @@
 for /f "delims=" %%i in ('cd') do set output=%%i
 set CARGO_TARGET_DIR=%output%
 cd ..\src\rust
-cargo build
+cargo build %1 --features "hardsubx_ocr" --target x86_64-pc-windows-msvc
 cd ..\..\windows
-copy debug\ccx_rust.lib .\ccx_rust.lib
+IF "%~1"=="-r" (
+copy x86_64-pc-windows-msvc\release\ccx_rust.lib .\ccx_rust.lib
+) ELSE (
+copy x86_64-pc-windows-msvc\debug\ccx_rust.lib .\ccx_rust.lib
+)
