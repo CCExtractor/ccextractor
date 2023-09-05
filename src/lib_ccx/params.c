d5d3b8be1f078de5b6a8838dabe49adc0bdd42a1
Prateek Sunal <prtksunal@gmail.com>
2023-09-06 00:56:03 +0530

    fix: -h

--- src/lib_ccx/params.c
+++ src/lib_ccx/params.c
@@ -1228,7 +1228,7 @@ int parse_parameters(struct ccx_s_options *opt, int argc, char *argv[])
 {
 	for (int i = 1; i < argc; i++)
 	{
-		if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "--h") == 0)
+		if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
 		{
 			print_usage();
 			return EXIT_WITH_HELP;
