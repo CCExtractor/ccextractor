3e7ba4fe01c54e411e6e6907e81c06b8e4980f1b
Deepnarayan Sett <depnra1@gmail.com>
2025-03-30 14:08:46 +0530

    fixing format issues

--- src/rust/src/parser.rs
+++ src/rust/src/parser.rs
@@ -437,7 +437,7 @@ impl OptionsExt for Options {
         } else {
             0
         };
-        if num_input_files >= *inputfile_capacity as _ {
+        if num_input_files >= *inputfile_capacity as usize {
             *inputfile_capacity += 10;
         }
 
