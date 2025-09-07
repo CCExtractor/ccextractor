ff2790db01be06626cf6958f11ec9ab7075c4b86
steel-bucket <depnra1@gmail.com>
2025-09-07 15:01:28 +0530

    Demuxer: Windows failing CI

--- src/lib_ccx/es_functions.c
+++ src/lib_ccx/es_functions.c
@@ -20,12 +20,13 @@ static int read_pic_data(struct bitstream *esstream);
 
 #define debug(...) ccx_common_logging.debug_ftn(CCX_DMT_VERBOSE, __VA_ARGS__)
 
-static uint8_t ccxr_search_start_code(struct bitstream *esstream);
-static uint8_t ccxr_next_start_code(struct bitstream *esstream);
-static int ccxr_read_seq_info(struct lib_cc_decode *ctx, struct bitstream *esstream);
-static int ccxr_read_gop_info(struct encoder_ctx *enc_ctx, struct lib_cc_decode *dec_ctx, struct bitstream *esstream, struct cc_subtitle *sub);
-static int ccxr_read_pic_info(struct encoder_ctx *enc_ctx, struct lib_cc_decode *dec_ctx, struct bitstream *esstream, struct cc_subtitle *sub);
-static int ccxr_read_eau_info(struct encoder_ctx *enc_ctx, struct lib_cc_decode *dec_ctx, struct bitstream *esstream, int udtype, struct cc_subtitle *sub);
+extern uint8_t ccxr_search_start_code(struct bitstream *esstream);
+extern uint8_t ccxr_next_start_code(struct bitstream *esstream);
+extern int ccxr_read_seq_info(struct lib_cc_decode *ctx, struct bitstream *esstream);
+extern int ccxr_read_gop_info(struct encoder_ctx *enc_ctx, struct lib_cc_decode *dec_ctx, struct bitstream *esstream, struct cc_subtitle *sub);
+extern int ccxr_read_pic_info(struct encoder_ctx *enc_ctx, struct lib_cc_decode *dec_ctx, struct bitstream *esstream, struct cc_subtitle *sub);
+extern int ccxr_read_eau_info(struct encoder_ctx *enc_ctx, struct lib_cc_decode *dec_ctx, struct bitstream *esstream, int udtype, struct cc_subtitle *sub);
+extern int ccxr_read_pic_data(struct bitstream *esstream);
 
 /* Process a mpeg-2 data stream with "length" bytes in buffer "data".
  * The number of processed bytes is returned.
@@ -1037,6 +1038,7 @@ static int extension_and_user_data(struct encoder_ctx *enc_ctx, struct lib_cc_de
 // will point to where we want to restart after getting more.
 static int read_pic_data(struct bitstream *esstream)
 {
+    return ccxr_read_pic_data(esstream);
 	debug("Read PIC Data\n");
 
 	uint8_t startcode = next_start_code(esstream);
