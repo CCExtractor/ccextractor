05adb5f47e06f2a7854d451a1e317e326dd44680
Chandragupt <chandraguptsingh1729@gmail.com>
2026-01-08 05:40:50 +0530

    snap: add website and source-code metadata

--- snap/local/run-ccextractor.sh
+++ snap/local/run-ccextractor.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+# Ensure runtime resolution of bundled libraries (FFmpeg / GPAC / PulseAudio)
+
 export LD_LIBRARY_PATH="$SNAP/usr/lib/x86_64-linux-gnu:\
 $SNAP/usr/lib/x86_64-linux-gnu/blas:\
 $SNAP/usr/lib/x86_64-linux-gnu/lapack:\
