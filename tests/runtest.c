845dafbb7473c41799e28995a41b964295687c19
workwithabhinav07 <workwithabhinav07@gmail.com>
2026-01-09 21:39:07 +0530

    Fix: Initialize ccx_options in runtest.c to prevent segfault

--- tests/runtest.c
+++ tests/runtest.c
@@ -1,9 +1,8 @@
 #include <check.h>
-
-#include "../src/lib_ccx/lib_ccx.h"
+// FIXED: Added this header so we can use init_options()
 #include "../src/lib_ccx/ccx_common_option.h"
+#include "../src/lib_ccx/lib_ccx.h"
 
-// TESTS:
 #include "ccx_encoders_splitbysentence_suite.h"
 
 struct ccx_s_options ccx_options;
@@ -15,8 +14,13 @@ int main(void)
 	Suite *s;
 	SRunner *sr;
 
+	// FIXED: Initialize options to defaults to prevent crashes
+	init_options(&ccx_options);
+
 	s = ccx_encoders_splitbysentence_suite();
 	sr = srunner_create(s);
+
+	// CK_NOFORK is fine for now
 	srunner_set_fork_status(sr, CK_NOFORK);
 
 	srunner_run_all(sr, CK_VERBOSE);
