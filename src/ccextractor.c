5d292c2fa56f581038b5f1715f6be9752e7049c9
mbro <mohithshuka@gmail.in>
2026-01-19 13:10:02 +0530

    UX: clarify message when no embedded captions are found in MP4 files

--- src/ccextractor.c
+++ src/ccextractor.c
@@ -404,8 +404,8 @@ int start_ccx()
 	dinit_libraries(&ctx);
 
 	if (!ret)
-		mprint("\nNo embedded captions were found in the input file.\n");
 		mprint("Note: Standard YouTube MP4 files usually do not contain embedded captions.\n");
+
 
 	print_end_msg();
 
