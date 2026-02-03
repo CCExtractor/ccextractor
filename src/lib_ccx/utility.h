52cf55479ab28bd227a42eaa80d081781f7a1f7a
Vivian Rodrigues <vivianrodrigues@Vivians-Laptop.local>
2026-02-03 12:30:54 +0530

    Resolve merge conflicts: utility.h and Cargo.toml
    - utility.h: keep Rust forward declarations and DISABLE_RUST block with SRT exports; keep rest from master
    - Cargo.toml: keep libc from rust_encoder_upd2; keep serial_test, encoding_rs 0.8.35, and rsmpeg target deps from master

--- src/lib_ccx/utility.h
+++ src/lib_ccx/utility.h
@@ -2,7 +2,7 @@
 #define CC_UTILITY_H
 #include <signal.h>
 #ifndef _WIN32
-	#include <arpa/inet.h>
+#include <arpa/inet.h>
 #endif
 
 #ifndef MIN
@@ -11,12 +11,12 @@
 
 #define RL32(x) (*(unsigned int *)(x))
 #define RB32(x) (ntohl(*(unsigned int *)(x)))
-#define RL16(x) (*(unsigned short int*)(x))
-#define RB16(x) (ntohs(*(unsigned short int*)(x)))
+#define RL16(x) (*(unsigned short int *)(x))
+#define RB16(x) (ntohs(*(unsigned short int *)(x)))
 
-#define RB24(x) (  ((unsigned char*)(x))[0] << 16 | ((unsigned char*)(x))[1] << 8 | ((unsigned char*)(x))[2]  )
+#define RB24(x) (((unsigned char *)(x))[0] << 16 | ((unsigned char *)(x))[1] << 8 | ((unsigned char *)(x))[2])
 
-#define CCX_NOPTS	((int64_t)UINT64_C(0x8000000000000000))
+#define CCX_NOPTS ((int64_t)UINT64_C(0x8000000000000000))
 
 struct ccx_rational
 {
@@ -45,10 +45,10 @@ extern int ccxr_write_cc_subtitle_as_srt(struct cc_subtitle *sub, struct encoder
 extern int ccxr_write_cc_buffer_as_srt(struct eia608_screen *data, struct encoder_ctx *context);
 #endif
 
-int levenshtein_dist_char (const char *s1, const char *s2, unsigned s1len, unsigned s2len);
-void init_boundary_time (struct ccx_boundary_time *bt);
-void print_error (int mode, const char *fmt, ...);
-int stringztoms (const char *s, struct ccx_boundary_time *bt);
+int levenshtein_dist_char(const char *s1, const char *s2, unsigned s1len, unsigned s2len);
+void init_boundary_time(struct ccx_boundary_time *bt);
+void print_error(int mode, const char *fmt, ...);
+int stringztoms(const char *s, struct ccx_boundary_time *bt);
 char *get_basename(char *filename);
 const char *get_file_extension(const enum ccx_output_format write_format);
 char *create_outfilename(const char *basename, const char *suffix, const char *extension);
@@ -57,7 +57,7 @@ size_t utf16_to_utf8(unsigned short utf16_char, unsigned char *out);
 LLONG change_timebase(LLONG val, struct ccx_rational cur_tb, struct ccx_rational dest_tb);
 char *str_reallocncat(char *dst, char *src);
 
-void dump (LLONG mask, unsigned char *start, int l, unsigned long abs_start, unsigned clear_high_bit);
+void dump(LLONG mask, unsigned char *start, int l, unsigned long abs_start, unsigned clear_high_bit);
 LLONG change_timebase(LLONG val, struct ccx_rational cur_tb, struct ccx_rational dest_tb);
 void timestamp_to_vtttime(uint64_t timestamp, char *buffer);
 int vasprintf(char **strp, const char *fmt, va_list ap);
@@ -69,4 +69,4 @@ char *strtok_r(char *str, const char *delim, char **saveptr);
 
 void write_wrapped(int fd, const char *buf, size_t count);
 
-#endif //CC_UTILITY_H
+#endif // CC_UTILITY_H
