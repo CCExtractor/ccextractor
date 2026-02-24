874e603d546c123b3298427c70e9f15719bc1af8
Anayo Anyafulu <junioranyafulu@gmail.com>
2026-02-24 23:22:26 +0200

    Add Safety doc comment to ccxr_hex_to_int

--- src/rust/src/libccxr_exports/util.rs
+++ src/rust/src/libccxr_exports/util.rs
@@ -1,6 +1,16 @@
 use std::ffi::{c_char, c_int};
 
+/// Rust equivalent for `hex_to_int` function in C. Uses C-native types as input and output.
+///
+/// # Safety
+///
+/// `high` and `low` must be valid ASCII characters. Invalid characters will
+/// return -1 rather than causing undefined behavior.
 #[no_mangle]
 pub unsafe extern "C" fn ccxr_hex_to_int(high: c_char, low: c_char) -> c_int {
-    lib_ccxr::util::hex::hex_to_int(high as u8 as char, low as u8 as char).unwrap_or(-1)
+    lib_ccxr::util::hex::hex_to_int(
+        high as u8 as char,
+        low as u8 as char,
+    )
+    .unwrap_or(-1)
 }
