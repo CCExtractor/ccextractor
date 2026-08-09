CCExtractor/ccextractor

commit 03ad9e8e029ded37980c8b51f8c9450aedc5a6e5

[FEATURE] Allow output \0 terminated frames (for WebSocket streaming support) (#2105)
* [feat] Allow output \0 terminated frames

* Fix rust `FromCType`

* use encoded_end_frame for text-based captions

* add changelog entry

* fix CEA-708 Rust decoder

* fix Rust formating

* remove unused `crlf` field - satisfy clippy function argument limit

* silence clippy function argument limit in `Writer`

* Fix writing frame end with multiline captions

* fix formatting errors

src/lib_ccx/ccx_encoders_common.h
@@ -146,6 +146,8 @@ struct encoder_ctx
 	unsigned int encoded_crlf_length;
 	unsigned char encoded_br[16];
 	unsigned int encoded_br_length;
+	unsigned char encoded_end_frame[16];
+	unsigned int encoded_end_frame_length;
 
 	// MCC File
 	int header_printed_flag;

src/lib_ccx/ccx_encoders_transcript.c
@@ -92,7 +92,7 @@ int write_cc_bitmap_as_transcript(struct cc_subtitle *sub, struct encoder_ctx *c
 				}
 			}
 
-			write_wrapped(context->out->fh, context->encoded_crlf, context->encoded_crlf_length);
+			write_wrapped(context->out->fh, context->encoded_end_frame, context->encoded_end_frame_length);
 		}
 	}
 #endif
@@ -134,6 +134,7 @@ int write_cc_subtitle_as_transcript(struct cc_subtitle *sub, struct encoder_ctx 
 
 		str = sub->data;
 
+		int wrote_something = 0;
 		str = strtok_r(str, "\r\n", &save_str);
 		do
 		{
@@ -141,6 +142,15 @@ int write_cc_subtitle_as_transcript(struct cc_subtitle *sub, struct encoder_ctx 
 			if (length <= 0)
 			{
 				continue;
+			}
+
+			if (wrote_something)
+			{
+				ret = write(context->out->fh, context->encoded_crlf, context->encoded_crlf_length);
+				if (ret < context->encoded_crlf_length)
+				{
+					mprint("Warning:Loss of data\n");
+				}
 			}
 
 			if (context->transcript_settings->showStartTime)
@@ -200,13 +210,15 @@ int write_cc_subtitle_as_transcript(struct cc_subtitle *sub, struct encoder_ctx 
 				mprint("Warning:Loss of data\n");
 			}
 
-			ret = write(context->out->fh, context->encoded_crlf, context->encoded_crlf_length);
-			if (ret < context->encoded_crlf_length)
-			{
-				mprint("Warning:Loss of data\n");
-			}
+			wrote_something = 1;
 
 		} while ((str = strtok_r(NULL, "\r\n", &save_str)));
+
+		ret = write(context->out->fh, context->encoded_end_frame, context->encoded_end_frame_length);
+		if (ret < context->encoded_end_frame_length)
+		{
+			mprint("Warning:Loss of data\n");
+		}
 
 		freep(&sub->data);
 		lsub = sub;
@@ -321,18 +333,13 @@ void write_cc_line_as_transcript2(struct eia608_screen *data, struct encoder_ctx
 		{
 			mprint("Warning:Loss of data\n");
 		}
-
-		ret = write(context->out->fh, context->encoded_crlf, context->encoded_crlf_length);
-		if (ret < context->encoded_crlf_length)
-		{
-			mprint("Warning:Loss of data\n");
-		}
 	}
 	// fprintf (wb->fh,encoded_crlf);
 }
 
 int write_cc_buffer_as_transcript2(struct eia608_screen *data, struct encoder_ctx *context)
 {
+	int ret;
 	int wrote_something = 0;
 	dbg_print(CCX_DMT_DECODER_608, "\n- - - TRANSCRIPT caption - - -\n");
 
@@ -340,10 +347,29 @@ int write_cc_buffer_as_transcript2(struct eia608_screen *data, struct encoder_ct
 	{
 		if (data->row_used[i])
 		{
+			if (wrote_something)
+			{
+				ret = write(context->out->fh, context->encoded_crlf, context->encoded_crlf_length);
+				if (ret < context->encoded_crlf_length)
+				{
+					mprint("Warning:Loss of data\n");
+				}
+			}
+
 			write_cc_line_as_transcript2(data, context, i);
+			wrote_something = 1;
 		}
-		wrote_something = 1;
 	}
+
+	if (wrote_something)
+	{
+		ret = write(context->out->fh, context->encoded_end_frame, context->encoded_end_frame_length);
+		if (ret < context->encoded_end_frame_length)
+		{
+			mprint("Warning:Loss of data\n");
+		}
+	}
+
 	dbg_print(CCX_DMT_DECODER_608, "- - - - - - - - - - - -\r\n");
 	return wrote_something;
 }

src/lib_ccx/ccx_common_option.c
@@ -134,6 +134,7 @@ void init_options(struct ccx_s_options *options)
 	options->enc_cfg.trim_subs = 0; // "	Remove spaces at sides?	"
 	options->enc_cfg.in_format = 1;
 	options->enc_cfg.line_terminator_lf = 0; // 0 = CRLF
+	options->enc_cfg.frame_terminator_0 = 0; // 0 = frames terminated by line_terminator_lf
 	options->enc_cfg.start_credits_text = NULL;
 	options->enc_cfg.end_credits_text = NULL;
 	options->enc_cfg.encoding = CCX_ENC_UTF_8;

src/lib_ccx/ccx_common_option.h
@@ -67,6 +67,7 @@ struct encoder_cfg
 	int no_type_setting;
 	int cc_to_stdout;	// If this is set to 1, the stdout will be flushed when data was written to the screen during a process_608 call.
 	int line_terminator_lf; // 0 = CRLF, 1=LF
+	int frame_terminator_0; // 0 = frames terminated by line_terminator_lf, 1 = frames terminated by \0
 	LLONG subs_delay;	// ms to delay (or advance) subs
 	int program_number;
 	unsigned char in_format;

src/lib_ccx/ccx_encoders_common.c
@@ -843,6 +843,17 @@ struct encoder_ctx *init_encoder(struct encoder_cfg *opt)
 
 	ctx->encoded_br_length = encode_line(ctx, ctx->encoded_br, (unsigned char *)"<br>");
 
+	if (opt->frame_terminator_0)
+	{
+		ctx->encoded_end_frame[0] = '\0';
+		ctx->encoded_end_frame_length = 1;
+	}
+	else
+	{
+		memcpy(ctx->encoded_end_frame, ctx->encoded_crlf, ctx->encoded_crlf_length + 1);
+		ctx->encoded_end_frame_length = ctx->encoded_crlf_length;
+	}
+
 	for (i = 0; i < ctx->nb_out; i++)
 		write_subtitle_file_header(ctx, ctx->out + i);
 

src/lib_ccx/params.c
@@ -347,6 +347,9 @@ void print_usage(void)
 	mprint("                       to the output file.\n");
 	mprint("                  --lf: Use LF (UNIX) instead of CRLF (DOS, Windows) as line\n");
 	mprint("                       terminator.\n");
+	mprint("     --null-terminated: Use \\0 instead of CRLF or LF for frame termination (see '--lf').\n");
+	mprint("                       e.g use '--txt --stdout --null-terminated' when piping to\n");
+	mprint("                       'websocat -0' (https://github.com/vi/websocat)\n");
 	mprint("                  --df: For MCC Files, force dropframe frame count.\n");
 	mprint("            --autodash: Based on position on screen, attempt to determine\n");
 	mprint("                       the different speakers and a dash (-) when each\n");

src/rust/lib_ccxr/src/common/options.rs
@@ -248,6 +248,7 @@ impl Default for EncoderConfig {
             no_type_setting: false,
             cc_to_stdout: false,
             line_terminator_lf: false,
+            frame_terminator_0: false,
             subs_delay: Timestamp::default(),
             program_number: 0,
             in_format: 1,
@@ -324,6 +325,8 @@ pub struct EncoderConfig {
     pub cc_to_stdout: bool,
     /// false = CRLF, true = LF
     pub line_terminator_lf: bool,
+    /// false = frames terminated by line_terminator_lf, true = frames terminated by \0
+    pub frame_terminator_0: bool,
     /// ms to delay (or advance) subs
     pub subs_delay: Timestamp,
     pub program_number: u32,

src/rust/src/args.rs
@@ -565,6 +565,9 @@ pub struct Args {
     /// terminator.
     #[arg(long, verbatim_doc_comment, help_heading=OUTPUT_AFFECTING_OUTPUT_FILES)]
     pub lf: bool,
+    /// Use \0 instead of CRLF or LF for frame termination (see '--lf')
+    #[arg(long, verbatim_doc_comment, help_heading=OUTPUT_AFFECTING_OUTPUT_FILES)]
+    pub null_terminated: bool,
     /// For MCC Files, force dropframe frame count.
     #[arg(long, verbatim_doc_comment, help_heading=OUTPUT_AFFECTING_OUTPUT_FILES)]
     pub df: bool,

commit 1782e17e8ceca232d8ab76bc5add0bc2f563c7bb

fix(transcript): skip encoded_end_frame when write_cc_line_as_transcript2 writes nothing
03ad9e8e refactored write_cc_buffer_as_transcript2 to write encoded_end_frame
at the end of each caption block. It set wrote_something = 1 unconditionally
after calling write_cc_line_as_transcript2, even when that function returned
without writing anything (blank cursor row, length = 0). Result: a bare newline
(encoded_end_frame) was emitted for every blank cursor row, producing spurious
blank lines in transcript output.

Fix: change write_cc_line_as_transcript2 from void to int (returns 1 if it
wrote, 0 if not) and use wrote_something |= to only set the flag when content
was actually emitted.

Fixes RT41 blank line regression introduced by PR #2105.

src/lib_ccx/ccx_encoders_common.h
@@ -238,7 +238,7 @@ void write_cc_buffer_to_gui(struct eia608_screen *data, struct encoder_ctx *cont
 int write_cc_buffer_as_g608(struct eia608_screen *data, struct encoder_ctx *context);
 int write_cc_buffer_as_transcript2(struct eia608_screen *data, struct encoder_ctx *context);
 
-void write_cc_line_as_transcript2(struct eia608_screen *data, struct encoder_ctx *context, int line_number);
+int write_cc_line_as_transcript2(struct eia608_screen *data, struct encoder_ctx *context, int line_number);
 
 int write_cc_subtitle_as_srt(struct cc_subtitle *sub, struct encoder_ctx *context);
 int write_cc_subtitle_as_ssa(struct cc_subtitle *sub, struct encoder_ctx *context);

src/lib_ccx/ccx_encoders_transcript.c
@@ -236,7 +236,7 @@ int write_cc_subtitle_as_transcript(struct cc_subtitle *sub, struct encoder_ctx 
 }
 
 // TODO Convert CC line to TEXT format and remove this function
-void write_cc_line_as_transcript2(struct eia608_screen *data, struct encoder_ctx *context, int line_number)
+int write_cc_line_as_transcript2(struct eia608_screen *data, struct encoder_ctx *context, int line_number)
 {
 	int ret = 0;
 	int length = get_str_basic(context->subline, data->characters[line_number],
@@ -255,7 +255,7 @@ void write_cc_line_as_transcript2(struct eia608_screen *data, struct encoder_ctx
 			// is set for example by the write_char function, it possible that we don't have one in empty lines (unclear)
 			// For now, let's not consider this a bug as before and just return.
 			// fatal (EXIT_BUG_BUG, "Bug in timedtranscript (ts_start_of_current_line==-1). Please report.");
-			return;
+			return 0;
 		}
 
 		if (context->transcript_settings->showStartTime)
@@ -333,7 +333,9 @@ void write_cc_line_as_transcript2(struct eia608_screen *data, struct encoder_ctx
 		{
 			mprint("Warning:Loss of data\n");
 		}
+		return 1;
 	}
+	return 0;
 	// fprintf (wb->fh,encoded_crlf);
 }
 
@@ -356,8 +358,7 @@ int write_cc_buffer_as_transcript2(struct eia608_screen *data, struct encoder_ct
 				}
 			}
 
-			write_cc_line_as_transcript2(data, context, i);
-			wrote_something = 1;
+			wrote_something |= write_cc_line_as_transcript2(data, context, i);
 		}
 	}
 